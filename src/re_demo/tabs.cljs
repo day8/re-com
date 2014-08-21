bd5a6a3d7bd0e10da204ebd9671b2dc30971c07b
mike-thompson-day8 <mike.thompson@day8.com.au>
2014-08-21 14:12:48 +1000

    WIP on tabs.  Add use of h-box

--- src/re_demo/tabs.cljs
+++ src/re_demo/tabs.cljs
@@ -1,10 +1,9 @@
 (ns re-demo.tabs
    (:require [reagent.core :as reagent]
              [alandipert.storage-atom :refer [local-storage]]
+             [re-com.box   :refer [h-box v-box box gap line]]
              [re-com.core  :refer [gap button]]
              [re-com.tabs  :as tabs]))
-
-
 
 
 ;; Define some tabs.
@@ -17,64 +16,86 @@
     {:id ::tab3  :label "Tab3"   :say-this  "I'm telling Mum on you two !!"}])
 
 
+(defn title
+  [text]
+  "An underlined, left justified, H3 Title"
+  [box
+   :size "auto"
+   :child  [v-box
+            :children [[h-box
+                        :children [[:h3 text]]]
+                       [line :size "1px"]]]])
+
+
 (defn horizontal-tabs-demo
   []
- (let [selected-tab-id (reagent/atom (:id (first tabs-definition)))]     ;; holds the id of the selected tab
-    (fn []                                                          ;; returning a function which closes over selected-tab-id.
-      [:div
-       [:h3.page-header "Horizontal Tabs"]
-       [:div.col-md-4
-        [:div.h4 "Notes:"]
-        [:ul
-         [:li "The code for this page can be found in /demo_src/tabs.cljs"]
-         [:li "For another demonstration, also look in /demo_src/core.cljs. This entire app is just a set of tabs."]
-         [:li "Tab-like controls can be styled in the ways shown to the right."]
-         [:li "We've linked all the examples to the one peice of state, so they'll change in lockstep."]
-         [:li "Also, notice that if you refresh the page, it remembers which tab you were in last time."]]]
+  (let [selected-tab-id (reagent/atom (:id (first tabs-definition)))]     ;; holds the id of the selected tab
+    (fn []
+      [v-box
+       :children [[title "Horizontal Tabs"]
+                  [h-box
+                   :gap "50px"
+                   :children [[v-box
+                               :margin  "20px 0px 0px 0px"       ;; TODO:  i supplied "start" (string) instead of :start and got runtime errors ... better protection
+                               :children [
+                                           [:div.h4 "Notes:"]
+                                           [:ul
+                                            [:li "The code for this page can be found in /demo_src/tabs.cljs"]
+                                            [:li "For another demonstration, also look in /demo_src/core.cljs. This entire app is just a set of tabs."]
+                                            [:li "Tab-like controls can be styled in the ways shown to the right."]
+                                            [:li "We've linked all the examples to the one peice of state, so they'll change in lockstep."]
+                                            [:li "Also, notice that if you refresh the page, it remembers which tab you were in last time."]]]]
 
-       [:div.col-md-7.col-md-offset-1
+                              [v-box
+                               :gap     "30px"
+                               :margin  "20px 0px 0px 0px"       ;; TOD0:  decide would we prefer to use :top-margin??
+                               :children [
+                                           ;; Three visual variations on tabs which share selection state via 'selected-tab-id'
+                                           [tabs/horizontal-pills
+                                            :model selected-tab-id
+                                            :tabs  tabs-definition]
 
-        ;; Three visual variations on tabs which share selection state via 'selected-tab-id'
-        [tabs/horizontal-pills
-          :model selected-tab-id
-          :tabs  tabs-definition]
-        [gap :height 40]
+                                           [tabs/horizontal-bar-tabs
+                                            :model selected-tab-id
+                                            :tabs  tabs-definition]
 
-        [tabs/horizontal-bar-tabs
-          :model selected-tab-id
-          :tabs  tabs-definition]
-        [gap :height 40]
+                                           [tabs/horizontal-tabs
+                                            :model selected-tab-id
+                                            :tabs  tabs-definition]
 
-        [tabs/horizontal-tabs
-          :model selected-tab-id
-          :tabs  tabs-definition]
-
-        ;; display the tab content which, in this case, is extracted from the tab definition
-        [:h4.well (:say-this (tabs/find-tab @selected-tab-id tabs-definition))]]])))
+                                           ;; display the tab content which, in this case, is extracted from the tab definition
+                                           [:h4.well (:say-this (tabs/find-tab @selected-tab-id tabs-definition))]]]]]]])))
 
 
 (defn remembers-demo
- []
- (let [tab-defs        [{:id ::1 :label "1"}
-                        {:id ::2 :label "2"}
-                        {:id ::3 :label "3" }]
-       id-store        (local-storage (atom nil) ::id-store)
-       selected-tab-id (reagent/atom (if  (nil? @id-store) (:id (first tab-defs)) @id-store))   ;; id of the selected tab
-       _               (add-watch selected-tab-id nil #(reset! id-store %4))]              ;; put into local-store for later
-    (fn []                                                          ;; returning a function which closes over selected-tab-id.
-      [:div
-       [:h3.page-header "A Remembered Tab"]
-       [:div.col-md-4
-        [:div.h4 "Notes:"]
-        [:ul
-         [:li "This tab's selection is remembered in local-storage."]
-         [:li "When you refresh the page, the selection persists."]
-         [:li "In contrast, the tabs above are volitile. If you switch to \"Welcome\" and back again, the current selection is lost. Easily fixed but be aware."]]]
+  []
+  (let [tab-defs        [{:id ::1 :label "1"}
+                         {:id ::2 :label "2"}
+                         {:id ::3 :label "3" }]
+        id-store        (local-storage (atom nil) ::id-store)
+        selected-tab-id (reagent/atom (if  (nil? @id-store) (:id (first tab-defs)) @id-store))   ;; id of the selected tab
+        _               (add-watch selected-tab-id nil #(reset! id-store %4))]                   ;; put into local-store for later
+    (fn []
+      [v-box
+       :children [[title "A Remembered Tab"]
+                  [h-box
+                   :gap "50px"
+                   :children [[v-box
+                               :margin  "20px 0px 0px 0px"       ;; TODO:  i supplied "start" (string) instead of :start and got runtime errors ... better protection
+                               :children [
+                                           [:div.h4 "Notes:"]
+                                           [:ul
+                                            [:li "This tab's selection is remembered in local-storage."]
+                                            [:li "When you refresh the page, the selection persists."]
+                                            [:li "Also, notice that if you refresh the page, it remembers which tab you were in last time."]]]]
 
-       [:div.col-md-7.col-md-offset-1
-        [tabs/horizontal-tabs
-          :model  selected-tab-id
-          :tabs  tab-defs]]])))
+                              [v-box
+                               :gap     "30px"
+                               :margin  "20px 0px 0px 0px"       ;; TOD0:  decide would we prefer to use :top-margin??
+                               :children [[tabs/horizontal-tabs
+                                           :model  selected-tab-id
+                                           :tabs  tab-defs]]]]]]])))
+
 
 (defn adding-tabs-demo
   []
@@ -114,11 +135,9 @@
 
 (defn panel
   []
-  [:div                             ;; must wrap
-   [horizontal-tabs-demo]
-   [gap :height 40]
-   [remembers-demo]
-   [gap :height 40]
-   [adding-tabs-demo]
-   [gap :height 40]
-   [vertical-tabs-demo]])
+  [v-box
+   ;; :gap "10px"
+   :children [[horizontal-tabs-demo]
+              [remembers-demo]
+              #_[adding-tabs-demo]
+              #_[vertical-tabs-demo]]])
