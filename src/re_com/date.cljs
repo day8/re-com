49eb1aaa0ce4222852cfcf2c35076a4893ea590b
hipitihop <anonymous.amphibian@gmail.com>
2014-09-16 09:59:12 +1000

    - static calendar header basic structure/css experiment

--- src/re_com/date.cljs
+++ src/re_com/date.cljs
@@ -10,13 +10,15 @@
 (defn single-date
   [& {:keys [model]}]
   "TODO: Render a bootstrap styled date calendar"
-  [:div {:class "calendar-date"}
+  [:div {:class "calendar-date daterangepicker opensleft show-calendar"
+         :style {:font-size "13px"}
+         }
    [:table {:class "table-condensed"}
     [:thead
      [:tr
       [:th {:class "prev available"}
        [:i {:class "fa fa-arrow-left icon-arrow-left glyphicon glyphicon-arrow-left"}]]
-      [:th {:class "month"} "Aug 2014"]
+      [:th {:class "month" :colSpan "5"} "Aug 2014"]
       [:th {:class "next available"}
        [:i {:class "fa fa-arrow-right icon-arrow-right glyphicon glyphicon-arrow-right"}]]]
      [:tr [:th "Su"][:th "Mo"][:th "Tu"][:th "We"][:th "Th"][:th "Fr"][:th "Sa"]]
