1903bc9e14b939c30d68fc1ab049aa28a32b7fa3
hipitihop <anonymous.amphibian@gmail.com>
2014-09-16 18:04:32 +1000

    - add project dependency for com.andrewmcveigh/cljs-time - base plumbing for calendar monthd header based on current date and inc/dec callbacks

--- src/re_com/date.cljs
+++ src/re_com/date.cljs
@@ -1,26 +1,54 @@
 (ns re-com.date
+  (:require-macros [clairvoyant.core :refer [trace-forms]])
   (:require
-    [re-com.util  :as util]
-    [clojure.string :as string]
-    [reagent.core :as reagent]))
+    [clairvoyant.core     :refer [default-tracer]]
+    [cljs-time.core       :refer [now minus plus months]]
+    [cljs-time.predicates :refer [sunday?]]
+    [cljs-time.format     :refer [parse unparse formatters formatter]]
+    [re-com.util          :as util]
+    [clojure.string       :as string]
+    [reagent.core         :as reagent]))
+
+;; --- private cljs-time facades ----------------------------------------------
+
+(def ^:private month-format (formatter "MMM yyyy"))
+
+(defn ^:private month-label [date] (unparse month-format date))
+
+(defn ^:private dec-date [date] (minus date (months 1)))
+
+(defn ^:private inc-date [date] (plus date (months 1)))
+
+;; ----------------------------------------------------------------------------
 
 ;; Based on: https://github.com/dangrossman/bootstrap-daterangepicker
-;; depends: css/daterangepicker-bs3.css
+;; depends: daterangepicker-bs3.css
+
+(def ^:private calendar-div
+  [:div
+   {:class "calendar-date daterangepicker opensleft show-calendar"
+    ;; override inherrited body larger 14px font-size
+    ;; override positioning form css because we are inline
+    ;; override specify width because currently it does not size on content GR investigatings
+    :style {:font-size "13px" :position "static" :width "195px"}}])
+
+(defn ^private table-thead
+  "Answer 2 x rows for month with nav buttons and days"
+  [current]
+  [:thead
+   [:tr
+    [:th {:class "prev available"}
+     [:i {:class "fa fa-arrow-left icon-arrow-left glyphicon glyphicon-arrow-left"
+          :on-click #(reset! current (dec-date @current))}]]
+    [:th {:class "month" :col-span "5"} (month-label @current)]
+    [:th {:class "next available"}
+     [:i {:class "fa fa-arrow-right icon-arrow-right glyphicon glyphicon-arrow-right"
+          :on-click #(reset! current (inc-date @current))}]]]
+   [:tr [:th "Su"][:th "Mo"][:th "Tu"][:th "We"][:th "Th"][:th "Fr"][:th "Sa"]]])
+
 
 (defn single-date
   [& {:keys [model]}]
-  "TODO: Render a bootstrap styled date calendar"
-  [:div {:class "calendar-date daterangepicker opensleft show-calendar"
-         :style {:font-size "13px"}
-         }
-   [:table {:class "table-condensed"}
-    [:thead
-     [:tr
-      [:th {:class "prev available"}
-       [:i {:class "fa fa-arrow-left icon-arrow-left glyphicon glyphicon-arrow-left"}]]
-      [:th {:class "month" :col-span "5"} "Aug 2014"]
-      [:th {:class "next available"}
-       [:i {:class "fa fa-arrow-right icon-arrow-right glyphicon glyphicon-arrow-right"}]]]
-     [:tr [:th "Su"][:th "Mo"][:th "Tu"][:th "We"][:th "Th"][:th "Fr"][:th "Sa"]]
-    ]
-   ]])
+  (let [current (reagent/atom (now))
+        table   [:table {:class "table-condensed"} [table-thead current]]]
+    (conj calendar-div table)))
--- src/re_demo/date.cljs
+++ src/re_demo/date.cljs
@@ -2,15 +2,21 @@
   (:require [re-demo.util    :refer  [title]]
             [re-com.core     :refer  [label]]
             [re-com.date     :refer  [single-date]]
-            [re-com.box      :refer  [h-box v-box box gap line]]
+            [re-com.box      :refer  [h-box v-box box gap line border]]
             [reagent.core    :as     reagent]))
 
 (defn inline-date
   []
+  ;;TODO: Greg investigating why border doesn't size based on content
+  ;; so box needs 2px bigger size.
   [v-box
    :gap "20px"
    :children [[title "Date"]
-              [single-date]]])
+              [box
+               :width "197px"
+               :child [border
+                       :radius "4px"
+                       :child [single-date]]]]])
 
 (defn inline-date-range
   []
