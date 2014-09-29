85c28df17d93dd0f26882026b02a07bbc9d20adb
mike.hughes <mike.hughes@day8.com.au>
2014-09-29 17:19:52 +1000

    Time range models still need some work.

--- src/re_com/time.cljs
+++ src/re_com/time.cljs
@@ -9,28 +9,22 @@
 
 ; --- Private functions ---
 
-(defrecord TimeRecord [hour minute])
-(defn create-time
-  "Return a TimeRecord. No validation is made for hours."
-  [& {:keys [hour minute]}]
-  (TimeRecord. hour minute))
+(defn time-int->hour-minute
+  "Convert the time integer (e.g. 930) to a vector of hour and minute."
+  [time-int]
+  (if (nil? time-int)
+    [nil nil]
+    [(quot time-int 100)
+     (rem time-int 100)]))
 
-(defn create-time-from-map
-  "Return a TimeRecord."
-  [tm-map]
-  (TimeRecord. (:hour tm-map) (:minute tm-map)))
-
-(defn time-from-int
-  "Return a TimeRecord."
-  [tm-int]
-  (TimeRecord. (quot tm-int 100) (rem tm-int 100)))
-
-(defn create-time-from-vector
+(defn time-integer-from-vector
   "Return a TimeRecord.
   ASSUMPTION: the vector contains 3 values which are -
    hour, ':' or '' and minutes."
   [vals]
-  (create-time :hour (first vals) :minute (last vals)))
+  (let [hr (if (nil? (first vals)) 0 (first vals))
+        mi (if (nil? (last vals)) 0 (last vals))]
+  (+ (* hr 100) mi)))
 
 (defn int-from-string
   [s]
@@ -41,111 +35,82 @@
         nil
         val))))
 
-(defn create-time-from-string
+(defn string->time-integer
   "Return a TimeRecord from the passed string."
   [s]
   (let [matches (re-matches #"^(\d{0,2})()()$|^(\d{0,1})(:{0,1})(\d{0,2})$|^(\d{0,2})(:{0,1})(\d{0,2})$" s)
        vals (filter #(not (nil? %))(rest matches))]
-    (create-time-from-vector (map int-from-string vals))))
+    (time-integer-from-vector (map int-from-string vals))))
 
-(defn time-record->string
-  "Return a string to display the time."
-  [time-record]
-  (str (when (:hour time-record)
-         (if (>= (:hour time-record) 10)
-           (:hour time-record)
-           (str (pad-zero-number (:hour time-record) 2))))
-       (when (:minute time-record)
-         (str (pad-zero-number (:minute time-record) 2)))))
+(defn display-string
+  "Return a string display of the time."
+  [[hour minute]]
+  (if (and (nil? hour)(nil? minute))
+    ""
+    (str
+      (if hour
+        (pad-zero-number hour 2)
+        "00")
+      ":"
+      (if minute
+        (str (pad-zero-number minute 2))
+        "00"))))
 
-(defn time-record->int
-  "Return the time as a time integer."
-  [time-record]
-  (let [hr (if (nil? (:hour time-record)) 0 (:hour time-record))
-        mi (if (nil? (:minute time-record)) 0 (:minute time-record))]
-  (+ (* hr 100) mi)))
+(defn time-int->display-string
+  "Return a string display of the time integer."
+  [time-integer]
+  (if (nil? time-integer)
+    (display-string [nil nil])
+    (display-string (time-int->hour-minute time-integer))))
 
 ;; --- Validation ---
 
 (defn validate-hours
   "Validate the first element of a time vector. Return true if it is valid."
-  [time-record min max]
-  (let [hr (:hour time-record)]
+  [time-integer min max]
+  (let [hr (quot time-integer 100)]
     (if hr
-      (and (>= hr (:hour min))(<= hr (:hour max)))
+      (and (if (nil? min) true (>= hr (quot min 100)))(if (nil? max) true (<= hr (quot max 100))))
       true)))
 
 (defn validate-minutes
   "Validate the second element of a time vector. Return true if it is valid."
-  [time-record]
-  (let [mi (:minute time-record)]
+  [time-integer]
+  (let [mi (rem time-integer 100)]
     (if mi
       (< mi 60)
       true)))
 
 (defn validate-time-range
   "Validate the time in comparison to the min and max values. Return true if it is valid."
-  [time-record min max]
-  (let [tm-int (+ (* (:hour time-record) 100) (:minute time-record))
-        minimum (+ (* (:hour min) 100)(:minute min))
-        maximum (+ (* (:hour max) 100)(:minute max))]
-    (if (or (< tm-int minimum)
-            (> tm-int maximum))
-      false
-      true)))
+  [time-integer min max]
+  (and (if (nil? min) true (>= time-integer min))
+       (if (nil? max) true (<= time-integer max))))
 
-(defn validated-time-record
+(defn validated-time-integer
   "Validate the values in the vector.
   If any are invalid replace them and the following values with nil."
-  [time-record min max]
-  (let [tm-string   (str (:hour time-record) ":" (:minute time-record))
-        range-start (str (first min) ":" (last min))
-        range-end   (str (first max) ":" (last max))
-        range-str   (str range-start "-" range-end)]
-    (if-not (validate-hours time-record min max)
+  [time-integer min max]
+  (let [tm-string   (time-int->display-string time-integer)
+        range-str   (str (time-int->display-string min) "-" (time-int->display-string max))]
+    (if-not (validate-hours time-integer min max)
       (do
-        (create-time :hour nil :minute nil)
+        nil
         (.info js/console (str "WARNING: Time " tm-string " is outside range " range-str)))
-      (if-not (validate-minutes time-record)
+      (if-not (validate-minutes time-integer)
         (do
-          (create-time :hour (:hour time-record) :minute nil)
+          (time-integer-from-vector [(quot time-integer 100) 0])
           (.info js/console (str "WARNING: Minutes of " tm-string " are invalid.")))
-          time-record))))
+          time-integer))))
 
-(defn valid-time?
-  "Return true if the passed time is valid."
-  [time-record min max]
-  (if-not (validate-hours time-record min max)
+(defn valid-time-integer?
+  "Return true if the passed time integer is valid."
+  [time-integer min max]
+  (if-not (validate-hours time-integer min max)
     false
-    (if-not (validate-minutes time-record)
+    (if-not (validate-minutes time-integer)
       false
-      (validate-time-range time-record min max))))
-
-(defn character-valid?
-  "Return true if the character is valid."
-  [ch]
-  (not (nil? (re-matches #"^[\d|:]$" (str ch)))))
-
-(defn key-pressed
-  "Prevent input of invalid characters."
-  [ev]
-  (if (character-valid? (char (.-charCode ev)))
-    true
-    (do
-      (.info js/console (str "INFO: rejected keyboard input of char " (.-charCode ev)))
-      false)))
-
-(defn display-string
-  "Return a string display of the time."
-  [time-record]
-  (str
-    (if (:hour time-record)
-      (pad-zero-number (:hour time-record) 2)
-      "00")
-    ":"
-    (if (:minute time-record)
-      (str (pad-zero-number (:minute time-record) 2))
-      "00")))
+      (validate-time-range time-integer min max))))
 
 (defn got-focus
   "When the time input gets focus, select everything."
@@ -159,38 +124,39 @@
        vals (filter #(not (nil? %))(rest matches))]
     (= (count vals) 3)))  ;; Cannot do any further validation here - input must be finished first (why? because when entering 6:30, "63" is not valid)
 
+(defn validate-max-min
+  [minimum-time maximum-time]
+  (if-not (valid-time-integer? minimum-time nil nil)
+    (throw (js/Error. (str "minimum " minimum-time " is not a valid time integer."))))
+  (if-not (valid-time-integer? maximum-time nil nil)
+    (throw (js/Error. (str "maximum " maximum-time " is not a valid time integer."))))
+  (if (and minimum-time maximum-time)
+    (if-not (< minimum-time maximum-time)
+      (throw (js/Error. (str "maximum " maximum-time " is less than minimum " minimum-time "."))))))
+
 (defn time-changed
   [ev tmp-model min max]
-  (let [target (.-target ev)
-        input-val (.-value target)
-        valid? (validate-string input-val min max)]
+  (let [input-val (-> ev .-target .-value)
+        valid? (validate-string input-val @min @max)]
     (when valid?
       (reset! tmp-model input-val))))
 
 (defn time-updated
-  "Check what has been entered is complete. If not, and if possible, complete it. Then update the model."
-  [ev model tmp-model min max callback]
-  (let [target (.-target ev)
-        input-val (.-value target)
-        time-record (create-time-from-string input-val)]
-    (reset! tmp-model (time-record->int (validated-time-record time-record min max)))
-    ;;(set! (.-value target)(display-string @tmp-model)))
-  (if callback (callback (time-record->int @tmp-model)))))
+  "Check what has been entered is complete. Then update the model."
+  [ev tmp-model min max callback]
+  (let [input-val (-> ev .-target .-value)
+        time-int (string->time-integer input-val)]
+    (reset! tmp-model (display-string (time-int->hour-minute (validated-time-integer time-int @min @max))))
+    ;;(set! (-> ev .-target .-value)(display-string @tmp-model))
+  (when callback (callback time-int))))
 
-(defn clipboard-paste
-  "Prevent pasting of invalid characters."
-  [ev tmp-model min max]
-  (let [data (.getData (.-clipboardData ev) "text/plain")
-        chrs (seq data)]
-    (if (every? #(character-valid? %) chrs)
-      (do
-        (let [time-record (create-time-from-string data)]
-          (reset! tmp-model (time-record->int (validated-time-record time-record min max))))  ;; TODO this is duplicated code
-        true)
-      (do
-        (.info js/console (str "INFO: rejected paste of '" data "'"))
-        false))))
-
+(defn atom-on
+  [model default]
+  (reagent/atom (if model
+                   (if (satisfies? cljs.core/IDeref model)
+                     @model
+                     model)
+                   default)))
 
 ;; --- Components ---
 
@@ -203,12 +169,15 @@
     maximum-time - default is 2359 - a time integer
     on-change - function to call when model has changed - parameter will be the new value
     style - css"
-  [& {:keys [model]}]
-  ;; TODO validate model, min & max  first
-  (let [tmp-model (reagent/atom (if (satisfies? cljs.core/IDeref model) @model model))]
+  [& {:keys [model minimum-time maximum-time]}]
+  (let [deref-model (if (satisfies? cljs.core/IDeref model) @model model)
+        tmp-model (atom-on (display-string (time-int->hour-minute deref-model)) "")
+        min (atom-on minimum-time 0)
+        max (atom-on maximum-time 2359)]
+    (validate-max-min @min @max)  ;; This will throw an error if the parameters are invalid
+    (if-not (valid-time-integer? deref-model @min @max)
+      (throw (js/Error. (str "model " deref-model " is not a valid time integer."))))
     (fn [& {:keys [model on-change minimum-time maximum-time style]}]
-      (let [min (if minimum-time (time-from-int minimum-time) (create-time :hour 0 :minute 0))
-            max (if maximum-time (time-from-int maximum-time) (create-time :hour 23 :minute 59))]
         [:span.input-append.bootstrap-timepicker
           [:input
             {:type "text"
@@ -221,9 +190,9 @@
              ;;:on-key-press #(key-pressed %)
              ;;:on-paste #(clipboard-paste % tmp-model min max)
              :on-change #(time-changed % tmp-model min max)
-             :on-blur #(time-updated % model tmp-model min max on-change)}
+             :on-blur #(time-updated % tmp-model min max on-change)}
             ;;[:span.add-on [:i.glyphicon.glyphicon-time]]
-           ]]))))
+           ]])))
 
 #_(defn atom-input [value]
   [:input {:type "text"
@@ -248,7 +217,7 @@
     gap - horizontal gap between time inputs - default '4px'
     style - css"
   [& {:keys [model]}]
-  (fn [& {:keys [model callback minimum-time maximum-time from-label to-label gap style]}]
+  (fn [& {:keys [model on-change minimum-time maximum-time from-label to-label gap style]}]
     (let [deref-model (if (satisfies? cljs.core/IDeref model) @model model)
           from-model  (reagent/atom (first deref-model))
           to-model    (reagent/atom (last  deref-model))]
@@ -257,14 +226,14 @@
         :children [(when from-label [:label from-label])
                    [time-input
                      :model from-model
-                     :callback callback
+                     :on-change on-change
                      :minimum-time minimum-time
-                     :maximum-time (last deref-model)
+                     :maximum-time to-model
                      :style style]
                    (when to-label [:label to-label])
                    [time-input
                      :model to-model
-                     :callback callback
-                     :minimum-time (first deref-model)
+                     :on-change on-change
+                     :minimum-time from-model
                      :maximum-time maximum-time
                      :style style]]])))
--- test/re-com/time_test.cljs
+++ test/re-com/time_test.cljs
@@ -4,26 +4,16 @@
             [reagent.core :as reagent]
             [re-com.time :as time]))
 
-(def default-min (time/TimeRecord. 0 0))
-(def default-max (time/TimeRecord. 23 59))
-(def alternate-min (time/TimeRecord. 6 0))
-(def alternate-max (time/TimeRecord. 21 59))
-
-(def time-6-30 (time/create-time :hour 6 :minute 30))
-(def time-5-30 (time/create-time :hour 5 :minute 30))
-(def time-23-59-59 (time/create-time :hour 23 :minute 59))
-
 (deftest test-is-valid
   (are [expected actual] (= expected actual)
-    true (time/valid-time? (time/TimeRecord. 0 nil) default-min default-max)
-    true (time/valid-time? (time/TimeRecord. 6 nil)  default-min default-max)
-    true (time/valid-time? (time/TimeRecord. 0 0)   default-min default-max)
-    true (time/valid-time? (time/TimeRecord. 1 30)  default-min default-max)
-    true (time/valid-time? (time/TimeRecord. 21 59) default-min default-max)
-    false (time/valid-time? (time/TimeRecord. 24 30) default-min default-max)
-    true  (time/valid-time? (time/TimeRecord. 14 30) alternate-min alternate-max)
-    false (time/valid-time? (time/TimeRecord. 4 30) alternate-min alternate-max)    ;; Before min
-    false (time/valid-time? (time/TimeRecord. 23 30) alternate-min alternate-max))) ;; After max
+    true (time/valid-time-integer? 0 0 2359)
+    true (time/valid-time-integer? 600  0 2359)
+    true (time/valid-time-integer? 130  0 2359)
+    true (time/valid-time-integer? 2159 0 2359)
+    false (time/valid-time-integer? 2430 0 2359)
+    true  (time/valid-time-integer? 1430 600 2200)
+    false (time/valid-time-integer? 430 600 2200)    ;; Before min
+    false (time/valid-time-integer? 2330 600 2200))) ;; After max
 
 (deftest test-time-from-int
   (are [expected actual] (= expected actual)
@@ -36,108 +26,92 @@
     1 (time/int-from-string "1")
     59 (time/int-from-string "59")))
 
-(deftest test-time-from-int
-  (are [expected actual] (= expected actual)
-    "00:00" (time/display-string (time/time-from-int 0))
-    "00:59" (time/display-string (time/time-from-int 59))
-    "09:55" (time/display-string (time/time-from-int 955))
-    "23:59" (time/display-string (time/time-from-int 2359))))
-
-(deftest test-time-record->int
-  (are [expected actual] (= expected actual)
-    0    (time/time-record->int (time/TimeRecord. nil nil))
-    0    (time/time-record->int (time/TimeRecord. 0 nil))
-    59   (time/time-record->int (time/TimeRecord. 0 59))
-    955  (time/time-record->int (time/TimeRecord. 9 55))
-    2359 (time/time-record->int (time/TimeRecord. 23 59))))
-
 (deftest test-display-string
   (are [expected actual] (= expected actual)
-    "00:00"    (time/display-string (time/TimeRecord. nil nil))
-    "00:00"    (time/display-string (time/TimeRecord. 0 nil))
-    "06:00"    (time/display-string (time/TimeRecord. 6 nil))
-    "00:00"    (time/display-string (time/TimeRecord. 0 0))
-    "01:30"    (time/display-string (time/TimeRecord. 1 30))
-    "21:59"    (time/display-string (time/TimeRecord. 21 59))
-    "24:30"    (time/display-string (time/TimeRecord. 24 30))))
+    ""         (time/display-string [nil nil])
+    "00:00"    (time/display-string [nil 0])
+    "06:00"    (time/display-string [6 0])
+    "00:00"    (time/display-string [0 0])
+    "01:30"    (time/display-string [1 30])
+    "21:59"    (time/display-string [21 59])
+    "24:30"    (time/display-string [24 30])))
 
-(deftest test-time-record->string
+(deftest test-time-int->display-string
   (are [expected actual] (= expected actual)
-    "00"      (time/time-record->string (time/TimeRecord. 0 nil))
-    "06"      (time/time-record->string (time/TimeRecord. 6 nil))
-    "11"      (time/time-record->string (time/TimeRecord. 11 nil))
-    "0900"    (time/time-record->string (time/TimeRecord. 9 0))
-    "0130"    (time/time-record->string (time/TimeRecord. 1 30))
-    "2159"    (time/time-record->string (time/TimeRecord. 21 59))
-    "2430"    (time/time-record->string (time/TimeRecord. 24 30))
-    "0000"    (time/time-record->string (time/TimeRecord. 0 0))))
+    ""       (time/time-int->display-string nil)
+    "00:00"  (time/time-int->display-string 0)
+    "06:00"  (time/time-int->display-string 600)
+    "11:00"  (time/time-int->display-string 1100)
+    "09:00"  (time/time-int->display-string 900)
+    "01:30"  (time/time-int->display-string 130)
+    "21:59"  (time/time-int->display-string 2159)
+    "24:30"  (time/time-int->display-string 2430)))
 
-(deftest test-create-time
-  (let [tm (time/create-time :hour 23 :minute 30)]
-    (are [expected actual] (= expected actual)
-      23 (:hour tm)
-      30 (:minute tm)))
-  (let [tm (time/create-time :hour 23 :minute 30)]
-    (are [expected actual] (= expected actual)
-      23 (:hour tm)
-      30 (:minute tm))))
+(deftest test-time-int->hour-minute
+  (are [expected actual] (= expected actual)
+    [nil nil]   (time/time-int->hour-minute nil)
+    [0 0]   (time/time-int->hour-minute 0)
+    [0 50]  (time/time-int->hour-minute 50)
+    [1 0]   (time/time-int->hour-minute 100)
+    [11 59] (time/time-int->hour-minute 1159)
+    [23 59] (time/time-int->hour-minute 2359)))
 
-(deftest test-create-time-from-map
-  (let [tm (time/create-time-from-map{:hour 23 :minute 45})]
-    (are [expected actual] (= expected actual)
-      23 (:hour tm)
-      45 (:minute tm))))
-
-(deftest test-create-time-from-string
-  (let [tm (time/create-time-from-string "6")]
-    (are [expected actual] (= expected actual)
-      6   (:hour tm)
-      nil (:minute tm)))
-  (let [tm (time/create-time-from-string "6:30")]
-    (are [expected actual] (= expected actual)
-      6   (:hour tm)
-      30  (:minute tm)))
-  (let [tm (time/create-time-from-string "630")]
-    (are [expected actual] (= expected actual)
-      6   (:hour tm)
-      30  (:minute tm)))
-  (let [tm (time/create-time-from-string "2252")]
-    (are [expected actual] (= expected actual)
-      22  (:hour tm)
-      52  (:minute tm)))
-  (let [tm (time/create-time-from-string "22:52")]
-    (are [expected actual] (= expected actual)
-      22  (:hour tm)
-      52  (:minute tm)))
-  (let [tm (time/create-time-from-string "6:60")] ;; Invalid values will be picked up later
-    (are [expected actual] (= expected actual)
-      6   (:hour tm)
-      60 (:minute tm))))
+(deftest test-string->time-integer
+  (are [expected actual] (= expected actual)
+    600   (time/string->time-integer "600")
+    630   (time/string->time-integer "630")
+    630   (time/string->time-integer "6:30")
+    630   (time/string->time-integer "06:30")
+    3000  (time/string->time-integer "30")
+    2359  (time/string->time-integer "2359")
+    2359  (time/string->time-integer "23:59")))
 
 (deftest test-validate-hours
   (are [expected actual] (= expected actual)
-    true  (time/validate-hours time-6-30 default-min default-max)
-    false (time/validate-hours (time/create-time :hour 5 :minute 30) alternate-min alternate-max)
-    true  (time/validate-hours nil default-min default-max)))
+    true  (time/validate-hours 630 0 2359)
+    false (time/validate-hours 530 600 2200)
+    true  (time/validate-hours nil 0 2359)))
 
 (deftest test-validate-minutes
   (are [expected actual] (= expected actual)
-    true (time/validate-minutes (time/create-time :hour 6 :minute nil))
-    true (time/validate-minutes (time/create-time :hour 6 :minute 55))
-    true (time/validate-minutes (time/create-time :hour 6 :minute 0))))
+    true (time/validate-minutes nil)
+    true (time/validate-minutes 655)
+    true (time/validate-minutes 600)
+    true (time/validate-minutes 2359)
+    false (time/validate-minutes 98)
+    false (time/validate-minutes 2398)))
 
 (deftest test-validated-time-record
   (are [expected actual] (= expected actual)
-    "06"      (time/time-record->string (time/validated-time-record (time/create-time :hour 6 :minute nil) default-min default-max))
-    "0630"    (time/time-record->string (time/validated-time-record time-6-30 default-min default-max))
-    "2359"    (time/time-record->string (time/validated-time-record (time/create-time :hour 23 :minute 59) default-min default-max))
-    ""        (time/time-record->string (time/validated-time-record (time/create-time :hour 5 :minute 0) alternate-min alternate-max))
-    ""        (time/time-record->string (time/validated-time-record (time/create-time :hour 23 :minute 59) alternate-min alternate-max))))
+    "06:00"   (time/time-int->display-string (time/validated-time-integer 600 0 2359))
+    "06:30"   (time/time-int->display-string (time/validated-time-integer 630 0 2359))
+    "23:59"   (time/time-int->display-string (time/validated-time-integer 2359 0 2359))
+    ""        (time/time-int->display-string (time/validated-time-integer 500 600 2200))
+    ""        (time/time-int->display-string (time/validated-time-integer 2315 600 2200))))
 
 (deftest test-validate-time-range
   (are [expected actual] (= expected actual)
-    true     (time/validate-time-range (time/create-time :hour 6 :minute 30) default-min default-max)
-    false    (time/validate-time-range (time/create-time :hour 24 :minute 30) default-min default-max)
-    false    (time/validate-time-range (time/create-time :hour 5 :minute 0) alternate-min alternate-max)
-    false    (time/validate-time-range (time/create-time :hour 23 :minute 45) alternate-min alternate-max)))
+    true     (time/validate-time-range 630 0 2359)
+    false    (time/validate-time-range 2430 0 2359)
+    false    (time/validate-time-range 500 600 2200)
+    false    (time/validate-time-range 2345 600 2200)))
 
+(deftest test-time-input
+ (is (fn? (time/time-input :model 1500)) "Expected a function.")
+ (is (fn? (time/time-input :model 1500 :minimum-time 600 :maximum-time 2159)) "Expected a function.")
+ (is (thrown? js/Error (time/time-input :model "abc") "should fail - model is invalid"))
+ (is (thrown? js/Error (time/time-input :model 930 :minimum-time "abc" :maximum-time 2159) "should fail - minimum is invalid"))
+ (is (thrown? js/Error (time/time-input :model 930 :minimum-time 600 :maximum-time "fred") "should fail - maximum is invalid"))
+ (is (thrown? js/Error (time/time-input :model 530 :minimum-time 600 :maximum-time 2159) "should fail - model is before range start"))
+ (is (thrown? js/Error (time/time-input :model 2230 :minimum-time 600 :maximum-time 2159) "should fail - model is after range end")))
+
+(deftest test-atom-on
+  (let [mdl (time/atom-on 123 nil)]
+    (is (satisfies? cljs.core/IDeref mdl) "Expected an atom.")
+    (is (= @mdl 123) "Expected value to be 123."))
+  (let [mdl (time/atom-on (reagent/atom 123) 456)]
+    (is (satisfies? cljs.core/IDeref mdl) "Expected an atom.")
+    (is (= @mdl 123) "Expected value to be 123."))
+  (let [mdl (time/atom-on nil 456)]
+    (is (satisfies? cljs.core/IDeref mdl) "Expected an atom.")
+    (is (= @mdl 456) "Expected value to be 456.")))
