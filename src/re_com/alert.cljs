31a9a35a8a0732923c8368a5487f19f0f13b1bcd
Gregg8 <gregg.ramsey@day8.com.au>
2014-09-30 15:53:44 +1000

    Many changes to alert and alert-list. Restructured the demo to be similar to dropdown 2.

--- src/re_com/alert.cljs
+++ src/re_com/alert.cljs
@@ -1,80 +1,75 @@
 (ns re-com.alert
-  (:require [reagent.core :as reagent]
-            [re-com.core  :refer [button]]
-            [re-com.util  :as util]))
+  (:require [re-com.core  :refer [button]]
+            [re-com.box   :refer [v-box scroller border]]
+            [re-com.util  :as    util]
+            [reagent.core :as    reagent]))
 
 ;;--------------------------------------------------------------------------------------------------
-;; Component: closeable-alert
-;;
-;;   Displays one Bootstrap alert box. A close button allows the message to be removed.
-;;   See function for more details
-;;
-;;   Notes/todo:
-;;    - Either validate alert-type or find a better way to specify the alert
+;; Component: alert
 ;;--------------------------------------------------------------------------------------------------
 
-(defn closeable-alert
-  [alert-item close-callback]
-  "Displays one Bootstrap alert box. A close button allows the message to be removed.
+(defn alert
+  [& {:keys [id alert-type heading body padding closeable on-close]}]
+  "Displays one alert box. A close button allows the message to be removed.
    Parameters:
-   - alert-item:       A map containing the definition of the alert:
-       - :id           A unique identifier, usually an integer or string
-       - :alert-type   A Bootstrap string determining the style. Either 'info', 'warning' or 'danger'
-       - :heading      Hiccup markup or a string containing the heading text
-       - :body         Hiccup markup or a string containing the body of the alert
-   - close-callback:   A callback function which knows how to close the alert"
-  (let [{:keys [id alert-type heading body]} alert-item]
-    (fn []
-      #_(util/console-log (str "in closeable-alert for id #" (:id alert-item)))
-      [:div.alert.fade.in {:class (str "alert-" alert-type)}
-       [button "×" #(close-callback id)
-        :class "close"]
-       [:h4 heading] ;; (str id ": " heading)
-       [:p body]])))
+    - :id           A unique identifier, usually an integer or string, but could be any complex data structure
+                    This is optional for single alerts. It's main use is in alert-list below
+    - :alert-type   A Bootstrap string determining the style. Either 'info', 'warning' or 'danger'
+    - :heading      Hiccup markup or a string containing the heading text
+    - :body         Hiccup markup or a string containing the body of the alert
+    - :padding      The amount of padding within the alert (default is 15px)
+    - :closeable    A boolean which determines if the close button is rendered (on-close must also be specified)
+    - :on-close     A callback function which knows how to close the alert"
+  [:div.alert.fade.in
+   {:class (str "alert-" alert-type)
+    :style {:flex "none" :padding (when padding padding)}}
+   (when (and closeable on-close)
+     [button
+      :label "×"
+      :on-click #(on-close id)
+      :class "close"])
+   (when heading [:h4 (when-not body {:style {:margin "0px"}}) heading])
+   (when body [:p body])])
 
 
 ;;--------------------------------------------------------------------------------------------------
 ;; Component: alert-list
-;;
-;;   To render this component, simply add the following hiccup where required:
-;;       [alert/alert-list]
-;;
-;;   To add a new alert to the list, for example:
-;;       (alert/add-alert "warning" {:heading "Hmmm, something might go wrong" :body "There be dragons!"})
-;;
-;;   Notes/todo:
-;;    - Currently uses internal storage for alerts
-;;    - User can click the close button of an alert to close it. Again handled internally
-;;    - Currently not currently generic. Only a single alert-list can be rendered
 ;;--------------------------------------------------------------------------------------------------
 
-(def alerts       (reagent/atom (sorted-map-by >)))
-(def alerts-count (reagent/atom 0))
-
-(defn close-alert
-  [id]
-  #_(util/console-log (str "closing alert #" id))
-  (swap! alerts dissoc id)
-  #_(util/console-log-prstr "after close" @alerts))
-
-
-(defn add-alert
-  [alert-type {:keys [heading body]}]
-  (let [id (swap! alerts-count inc)]
-    (swap! alerts assoc id {:id id :alert-type alert-type :heading heading :body body})
-    #_(util/console-log-prstr "after add" @alerts)))
-
-
 (defn alert-list
-  [alert-items close-callback] ;; TODO: Currently hard coded to interal version
-  "Displays a list of closeable-alerts
+  [& {:keys [alerts on-close border-style]}]
+  "Displays a list of alert components in a v-box
    Parameters:
-    - alert-items:      An atom containing a map of alerts. Normally (sorted-map-by >) so that latest alerts are rendered at the top
-    - close-callback:   A callback function which knows how to close an alert"
-  (fn []
-    #_(util/console-log "in alert-list")
-    [:div {:style {:border "1px dashed lightgrey"}}
-     (for [alert @alerts]
-       (do
-         #_(util/console-log (str "metadata :key=" (:id (last alert))))
-         ^{:key (:id (last alert))} [closeable-alert (last alert) close-alert]))]))
+    - :alerts       An atom containing a map of alerts. Would normally use (sorted-map-by >) so that latest alerts are
+                    rendered at the top. A typical map will look like this (substitute double quotes for single quotes):
+                      {2 {:alert-type 'warning'
+                          :heading 'Heading'
+                          :body 'Body'
+                          :padding '8px'
+                          :closeable true}
+                       1 {:alert-type 'info'
+                          :heading 'Heading'
+                          :body 'Body'}}
+    - :on-close     A callback function which knows how to close an alert based on the id passed to it.
+                    Usually something like this:
+                      #(swap! alerts dissoc %)
+    - :border-style The border style around the outside of the component. e.g. '1px dashed lightgrey' "
+  [border
+   :padding "4px"
+   :border  (when border-style border-style)
+   :child [scroller
+           :v-scroll :auto
+           :height "300px"
+           :child [v-box
+                   :size "auto"
+                   :children [(for [one-alert @alerts]
+                                (let [id (first one-alert)
+                                      {:keys [alert-type heading body padding closeable]} (last one-alert)]
+                                  ^{:key id} [alert
+                                              :id id
+                                              :alert-type alert-type
+                                              :heading heading
+                                              :body body
+                                              :padding padding
+                                              :closeable closeable
+                                              :on-close on-close]))]]]])
--- src/re_demo/alerts.cljs
+++ src/re_demo/alerts.cljs
@@ -1,19 +1,138 @@
 (ns re-demo.alerts
-  (:require [re-com.util    :as     util]
-            [re-com.core    :refer  [button]]
-            [re-com.alert   :as     alert]
-            [reagent.core   :as     reagent]))
+  (:require [re-com.util     :as     util]
+            [re-com.core     :refer [button label input-text checkbox]]
+            [re-com.box      :refer [h-box v-box box gap]]
+            [re-com.dropdown :refer [single-dropdown find-option filter-options-by-keyword]]
+            [re-com.alert    :refer [alert alert-list]]
+            [reagent.core    :as     reagent]))
 
 
-(comment
-  (add-alert "danger" {:heading "Unfortunately something bad happened" :body "Next time you should take more care! Next time you should take more care! Next time you should take more care! Next time you should take more care! Next time you should take more care!"})
-  (add-alert "info" {:heading "Here's some info for you" :body "The rain in Spain falls mainly on the plain"})
-  (add-alert "warning" {:heading "Hmmm, something might go wrong" :body "There be dragons!"})
-  (add-alert "info" {:heading "Here's some info for you" :body "The rain in Spain falls mainly on the plain"})
-  )
+(def demos [{:id 1 :label "Single alert with close button"}
+            {:id 2 :label "More sample alerts"}
+            {:id 3 :label "Alert list"}])
+
+
+(defn demo1
+  []
+  (let [show-alert (reagent/atom true)]
+    (fn []
+      [:div
+       [:p "The alert below is of type 'info' and includes a heading, body text and a close button to remove it."]
+       (if @show-alert
+         [alert
+          :id 1
+          :alert-type "info"
+          :heading    "Sample Alert Heading"
+          :body "This is the body of an info-styled alert. Click the x to close it."
+          :closeable  true
+          :on-close   #(reset! show-alert false)]
+         [:p {:style {:text-align "center" :margin "30px"}} "[You closed me]"])])))
+
+
+(defn demo2
+  []
+  (let [show-alert1 (reagent/atom true)
+        show-alert2 (reagent/atom true)]
+    (fn []
+      [:div
+       (when @show-alert1
+         [:div
+          [:p "The alert below has a heading but no body, allowing it to really stand out."]
+          [alert
+           :alert-type "info"
+           :heading "Information Heading"
+           :closeable true
+           :on-close #(reset! show-alert1 false)]])
+       (when @show-alert2
+         [:div
+          [:p "The warning-styled alert below only has a body so takes up a minimum amount of space (padding set to 4px)."]
+          [alert
+           :alert-type "warning"
+           :body "This is the body of an warning-styled alert."
+           :padding "4px"
+           :closeable true
+           :on-close #(reset! show-alert2 false)]])
+       [:p "The alert below has is a danger-styled alert. The closeable parameter is omitted so it doesn't have a close button.
+            It also contains a link to demonstrate the general markup capability."]
+       [alert
+        :alert-type "danger"
+        :heading    "Danger"
+        :body [:span "This is the body of an danger-styled alert. " [:a {:href "http://google.com" :target "_blank"} "Link to Google"]]]])))
+
+
+(defn add-alert
+  [alerts alerts-count alert-type {:keys [heading body]}]
+  (let [id (swap! alerts-count inc)]
+    (swap! alerts assoc id {:alert-type alert-type :heading heading :body body :padding "8px" :closeable true})))
+
+
+(defn demo3
+  []
+  (let [alerts       (reagent/atom (sorted-map-by >))
+        alerts-count (reagent/atom 0)]
+    (add-alert alerts alerts-count "danger" {:heading "Unfortunately something bad happened" :body "Next time you should take more care! Next time you should take more care! Next time you should take more care! Next time you should take more care! Next time you should take more care!"})
+    (add-alert alerts alerts-count "info" {:heading "Here's some info" :body "The rain in Spain falls mainly on the plain"})
+    (add-alert alerts alerts-count "warning" {:heading "Hmmm, something might go wrong" :body "There be dragons!"})
+    (add-alert alerts alerts-count "info" {:heading "Here's some info" :body "The rain in Spain falls mainly on the plain"})
+    (fn []
+      [v-box
+       :gap "10px"
+       :children [[:p "The component below is an alert-list. It can contain any number of alerts, with the most recently added alert at the top.
+                       Press the 'Add alert' button to add some more. It is wrapped in a scroller (height 300px) and a border. Padding is set to 8px."]
+                  [alert-list
+                   :alerts       alerts
+                   :on-close     #(swap! alerts dissoc %)
+                   :border-style "1px dashed lightgrey"]
+                  [button
+                   :label "Add alert"
+                   :style {:width "100px"}
+                   :on-click #(add-alert alerts alerts-count "info" {:heading (str "New alert #" @alerts-count) :body "This alert was added when the button below was clicked."})]]])))
+
+
+(defn notes
+  []
+  [v-box
+   :width    "500px"
+   :children [[:div.h4 "General notes:"]
+              [:ul
+               [:li "To create an alert component, the following parameter is required:"
+                [:ul
+                 [:li.spacer [:strong ":alert-type"] " - a Bootstrap CSS string determining the style. Either \"info\", \"warning\" or \"danger\"."]]]
+               [:li "The rest of the parameters are optional:"
+                [:ul
+                 [:li.spacer [:strong ":id"] " - A unique identifier, usually an integer or string. This is optional for single alerts. It's main use is in alert-list component."]
+                 [:li.spacer "Note: Although heading and body are optional, you really need to specify at least one of them."]
+                 [:li.spacer [:strong ":heading"] " - the heading section (hiccup markup or a string)."]
+                 [:li.spacer [:strong ":body"] " - the body of the alert (hiccup markup or a string)."]
+                 [:li.spacer [:strong ":padding"] " - the amount of padding within the alert (default is 15px)."]
+                 [:li.spacer [:strong ":closeable"] " - A boolean which determines if the close button is rendered (on-close must also be specified)."]
+                 [:li.spacer [:strong ":on-close"] " - A callback function which knows how to close the alert."]]]]]])
+
 
 (defn panel
   []
-  [:div
-   [:h3.page-header "Alerts"]
-   ])
+  (let [selected-demo-id (reagent/atom 1)]
+    (fn []
+      [v-box
+       :children [[:h3.page-header "Alerts"]
+                  [h-box
+                   :gap      "50px"
+                   :children [[notes]
+                              [v-box
+                               :gap       "15px"
+                               :size      "auto"
+                               :min-width "500px"
+                               :children  [[h-box
+                                            :gap      "10px"
+                                            :align    :center
+                                            :children [[label :label "Select a demo"]
+                                                       [single-dropdown
+                                                        :options   demos
+                                                        :model     selected-demo-id
+                                                        :width     "300px"
+                                                        :on-select #(reset! selected-demo-id %)]]]
+                                           [gap :size "0px"] ;; Force a bit more space here
+                                           (case @selected-demo-id
+                                             1 [demo1]
+                                             2 [demo2]
+                                             3 [demo3])]]]]]])))
