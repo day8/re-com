6820c233aea17427f9972963313f6a6a0fea81f5
Osbert Feng <osbert@iterinc.com>
2015-05-04 21:47:34 -0700

    Use Date instead of UtcDateTime for date-picker.
    This matches the state value to what is actually captured by the
    component. The component user can then construct a full date-time
    appropriate to their application semantics.
    
    Otherwise, in the case where the component user wants the local date,
    he/she must first convert the UTC date back to a local date.

--- src/re_com/datepicker.cljs
+++ src/re_com/datepicker.cljs
@@ -3,6 +3,7 @@
   (:require
     [reagent.core         :as    reagent]
     [cljs-time.core       :refer [now minus plus months days year month day day-of-week first-day-of-the-month before? after?]]
+    [cljs-time.coerce     :refer [to-date-time]]
     [cljs-time.local      :refer [local-now]]
     [re-com.validate      :refer [goog-date? css-style? html-attr?] :refer-macros [validate-args-macro]]
     [cljs-time.predicates :refer [sunday?]]
@@ -107,7 +108,7 @@
              {:style    {:font-size "24px"}
               :on-click (handler-fn (when prev-enabled? (reset! current prev-date)))}]]
 
-           [:th {:class "month" :col-span "5"} (month-label @current)]
+           [:th {:class "month" :col-span "5"} (month-label (to-date-time @current))]
 
            #_[:th {:class (str "next " (if next-enabled? "available selectable" "disabled"))} ;; TODO: Remove
             [:i {:class    "glyphicon glyphicon-chevron-right"
@@ -202,14 +203,14 @@
                        :today (local-now)})))
 
 (def datepicker-args-desc
-  [{:name :model        :required true                        :type "goog.date.UtcDateTime | atom"   :validate-fn goog-date? :description "the selected date. Should match :enabled-days"}
-   {:name :on-change    :required true                        :type "goog.date.UtcDateTime -> nil"   :validate-fn fn?        :description "called when a new selection is made"}
+  [{:name :model        :required true                        :type "goog.date.Date | atom"   :validate-fn goog-date? :description "the selected date. Should match :enabled-days"}
+   {:name :on-change    :required true                        :type "(goog.date.Date) -> nil" :validate-fn fn?        :description "called when a new selection is made"}
    {:name :disabled?    :required false :default false        :type "boolean | atom"                                         :description "when true, the can't select dates but can navigate"}
    {:name :enabled-days :required false                       :type "set"                            :validate-fn set?       :description "a subset of #{:Su :Mo :Tu :We :Th :Fr :Sa}. Only dates falling on these days will be user-selectable. Default is all 7 days"}
    {:name :show-weeks?  :required false :default false        :type "boolean"                                                :description "when true, week numbers are shown to the left"}
    {:name :show-today?  :required false :default false        :type "boolean"                                                :description "when true, today's date is highlighted"}
-   {:name :minimum      :required false                       :type "goog.date.UtcDateTime"          :validate-fn goog-date? :description "no selection or navigation before this date"}
-   {:name :maximum      :required false                       :type "goog.date.UtcDateTime"          :validate-fn goog-date? :description "no selection or navigation after this date"}
+   {:name :minimum      :required false                       :type "goog.date.DateTime"          :validate-fn goog-date? :description "no selection or navigation before this date"}
+   {:name :maximum      :required false                       :type "goog.date.DateTime"          :validate-fn goog-date? :description "no selection or navigation after this date"}
    {:name :hide-border? :required false :default false        :type "boolean"                                                :description "when true, the border is not displayed"}
    {:name :class        :required false                       :type "string"                         :validate-fn string?    :description "CSS class names, space separated"}
    {:name :style        :required false                       :type "CSS style map"                  :validate-fn css-style? :description "CSS styles to add or override"}
@@ -248,7 +249,7 @@
     :align :center
     :class "noselect"
     :children [[:label {:class "form-control dropdown-button"}
-                (unparse (if (seq format) (formatter format) date-format) @model)]
+                (unparse (if (seq format) (formatter format) date-format) (to-date-time @model))]
                #_[:span  {:class "dropdown-button activator input-group-addon"} ;; TODO: Remove
                 [:i {:class "glyphicon glyphicon-th"}]]
                [:span.dropdown-button.activator.input-group-addon
