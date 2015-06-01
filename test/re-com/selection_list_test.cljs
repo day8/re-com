974e1600807648db2a30fe2fb520c74b931238c0
mike.hughes <mike.hughes@day8.com.au>
2015-06-01 11:27:46 +1000

    Working on further tests.

--- test/re-com/selection_list_test.cljs
+++ test/re-com/selection_list_test.cljs
@@ -4,13 +4,18 @@
             [reagent.core :as reagent]
             [re-com.selection-list :as s-list]))
 
-
+;; --- Utilities ---
 
 (defn div-app []
   (let [div (.createElement js/document "div")]
     (set! (.-id div) "app")
     div))
 
+(extend-type js/NodeList
+  ISeqable
+  (-seq [array] (array-seq array 0)))
+
+;; --- Tests ---
 (deftest test-selection-list
   (let [new-comp (reagent/render-component
                    [s-list/selection-list
@@ -30,9 +35,31 @@
                     :choices [{:id "1" :name "item 1"} {:id "2" :name "item 2"} {:id "3" :name "item 3"}]
                     :model #{"2"}
                     :multi-select? false
-                    :on-change #(println %)]
+                    :on-change #()]
                    (div-app))
         props (last (-> new-comp .-_renderedComponent .-props .-argv .-tail))]
       (is (false? (:multi-select? props))  "Expected :multi-select? to default to true.")))
 
+
+(deftest test-selection-list-with-id-fn
+  (let [selected (reagent/atom #{"2"})
+        new-comp (reagent/render-component
+                   [s-list/selection-list
+                    :choices [{:id "1" :name "item 1"} {:id "2" :name "item 2"} {:id "3" :name "item 3"}]
+                    :model selected
+                    :label-fn :name
+                    ;;:id-fn identity
+                    :on-change #(print "selected: " %)]
+                   (div-app))
+        props       (last (-> new-comp .-_renderedComponent .-props .-argv .-tail))
+        dom-node    (.getDOMNode new-comp)
+        firstCkGrp  (-> dom-node .-childNodes first .-childNodes first .-childNodes first .-childNodes)
+        firstCk     (first firstCkGrp)
+        firstLbl    (second firstCkGrp)]
+      (is (ifn?  (:id-fn props))       "Expected :id-fn to be a function.")
+      (is (ifn?  (:label-fn props))    "Expected :label-fn to be a function.")
+      (is (= "item 1" (.-textContent firstLbl)) "Expected label for the first check box to use the :name of the element.")
+      ;; Removed the next two lines as the click event doesn't update the model. Checkbox shows 'checked' after the click but no on-change is triggered.
+      #_(.click firstCk)
+      #_(is (= #{"1"} @selected) "Expected set of selected values to match first element.")))
 
