3d1a15b57846199bd2d053b84a54f074f9631d99
mike.hughes <mike.hughes@day8.com.au>
2015-06-01 15:22:15 +1000

    Removed redundant test.

--- test/re-com/selection_list_test.cljs
+++ test/re-com/selection_list_test.cljs
@@ -41,25 +41,3 @@
       (is (false? (:multi-select? props))  "Expected :multi-select? to default to true.")))
 
 
-(deftest test-selection-list-with-id-fn
-  (let [selected (reagent/atom #{"2"})
-        new-comp (reagent/render-component
-                   [s-list/selection-list
-                    :choices [{:id "1" :name "item 1"} {:id "2" :name "item 2"} {:id "3" :name "item 3"}]
-                    :model selected
-                    :label-fn :name
-                    ;;:id-fn identity
-                    :on-change #(print "selected: " %)]
-                   (div-app))
-        props       (last (-> new-comp .-_renderedComponent .-props .-argv .-tail))
-        dom-node    (.getDOMNode new-comp)
-        firstCkGrp  (-> dom-node .-childNodes first .-childNodes first .-childNodes first .-childNodes)
-        firstCk     (first firstCkGrp)
-        firstLbl    (second firstCkGrp)]
-      (is (ifn?  (:id-fn props))       "Expected :id-fn to be a function.")
-      (is (ifn?  (:label-fn props))    "Expected :label-fn to be a function.")
-      (is (= "item 1" (.-textContent firstLbl)) "Expected label for the first check box to use the :name of the element.")
-      ;; Removed the next two lines as the click event doesn't update the model. Checkbox shows 'checked' after the click but no on-change is triggered.
-      #_(.click firstCk)
-      #_(is (= #{"1"} @selected) "Expected set of selected values to match first element.")))
-
