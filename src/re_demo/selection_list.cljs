c4c0d888f5c1fa9d241bcd30a36157ade61828d6
mike-thompson-day8 <mike.thompson@day8.com.au>
2015-07-17 12:49:59 +1000

    Mark selection list API as stable based on previous 7ca70c01f55221008072f390fffd53338cfde30a

--- src/re_demo/selection_list.cljs
+++ src/re_demo/selection_list.cljs
@@ -86,7 +86,7 @@
                            :gap      "10px"
                            :width    "450px"
                            :children [[title2 "Notes"]
-                                      [status-text "Alpha (API will change)" {:color "#EA6B00"}]
+                                      [status-text "Stable"]
                                       [p "Allows the user to select items from a list (single or multi)."]
                                       [p "Uses radio buttons when single selecting, and checkboxes when multi-selecting."]
                                       [p "Via strike-through, it supports the notion of selections representing exclusions, rather than inclusions."]
