354badcc8c20fd5dd0608d4f76206a0a52781fc8
Gregg8 <gregg.ramsey@day8.com.au>
2015-08-14 08:14:37 +1000

    Bumped version to 0.6.1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 
 ;; ---------------------------------------------------------------------------------------
 
-(defproject         re-com "0.6.0"
+(defproject         re-com "0.6.1"
   :description      "Reusable UI components for Reagent"
   :url              "https://github.com/Day8/re-com.git"
   :license          {:name "MIT"}
