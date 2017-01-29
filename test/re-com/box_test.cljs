8509dcff39d5b39c0ba6be04275aee7e3bc0f09d
Richard Russell <richardr39@gmail.com>
2017-01-29 11:48:13 +0000

    Add unit tests for some box and validate functions.

--- test/re-com/box_test.cljs
+++ test/re-com/box_test.cljs
@@ -2,15 +2,104 @@
   (:require-macros [cemerick.cljs.test :refer (is are deftest with-test run-tests testing test-var)])
   (:require [cemerick.cljs.test]
             [reagent.core :as reagent]
-            [re-com.box :refer [flex-child-style]]))
+            [re-com.box :as box]))
 
 (deftest test-flex-child-style
-         (are [expected actual] (= expected actual)
-              "initial"   (:flex (flex-child-style "initial"))
-              "auto"      (:flex (flex-child-style "auto"))
-              "none"      (:flex (flex-child-style "none"))
-              "0 0 100px" (:flex (flex-child-style "100px"))
-              "0 0 4.5em" (:flex (flex-child-style "4.5em"))
-              "60 1 0px"  (:flex (flex-child-style "60%"))
-              "60 1 0px"  (:flex (flex-child-style "60"))
-              "5 4 0%"    (:flex (flex-child-style "5 4 0%"))))
+  (are [expected actual] (= expected actual)
+    "initial"   (:flex (box/flex-child-style "initial"))
+    "auto"      (:flex (box/flex-child-style "auto"))
+    "none"      (:flex (box/flex-child-style "none"))
+    "0 0 100px" (:flex (box/flex-child-style "100px"))
+    "0 0 4.5em" (:flex (box/flex-child-style "4.5em"))
+    "60 1 0px"  (:flex (box/flex-child-style "60%"))
+    "60 1 0px"  (:flex (box/flex-child-style "60"))
+    "5 4 0%"    (:flex (box/flex-child-style "5 4 0%"))))
+
+(deftest test-flex-flow-style
+  (is (= (box/flex-flow-style "row wrap")
+         {:-webkit-flex-flow "row wrap"
+          :flex-flow "row wrap"})))
+
+(deftest test-justify-style
+  (let [make-expected (fn [x] {:-webkit-justify-content x
+                               :justify-content x})]
+    (are [expected actual] (= expected actual)
+      (make-expected "flex-start") (box/justify-style :start)
+      (make-expected "flex-end") (box/justify-style :end)
+      (make-expected "center") (box/justify-style :center)
+      (make-expected "space-between") (box/justify-style :between)
+      (make-expected "space-around") (box/justify-style :around))))
+
+(deftest test-align-style
+  (let [make-align-items (fn [x] {:-webkit-align-items x
+                                  :align-items x})]
+    (are [expected actual] (= expected actual)
+      (make-align-items "flex-start") (box/align-style :align-items :start)
+      (make-align-items "flex-end") (box/align-style :align-items :end)
+      (make-align-items "center") (box/align-style :align-items :center)
+      (make-align-items "baseline") (box/align-style :align-items :baseline)
+      (make-align-items "stretch") (box/align-style :align-items :stretch))))
+
+(deftest test-scroll-style
+  (are [expected actual] (= expected actual)
+    {:overflow "auto"} (box/scroll-style :overflow :auto)
+    {:overflow "hidden"} (box/scroll-style :overflow :off)
+    {:overflow "scroll"} (box/scroll-style :overflow :on)
+    {:overflow "visible"} (box/scroll-style :overflow :spill)))
+
+(deftest test-gap
+  (are [expected actual] (= expected actual)
+    [:div
+     {:class "rc-gap my-gap"
+      :style {:flex "0 0 1px"
+              :-webkit-flex "0 0 1px"}
+      :id "my-id"}]
+    (box/gap :class "my-gap" :attr {:id "my-id"} :size "1px")))
+
+(deftest test-line
+  (are [expected actual] (= expected actual)
+    [:div
+     {:class "rc-line my-line"
+      :style {:flex "0 0 1px"
+              :-webkit-flex "0 0 1px"
+              :background-color "lightgray"}
+      :id "my-id"}]
+    (box/line :class "my-line" :attr {:id "my-id"})))
+
+(deftest test-box
+  (are [expected actual] (= expected actual)
+    [:div
+     {:class "rc-box display-flex my-box"
+      :style {:flex "none"
+              :-webkit-flex "none"
+              :flex-flow "inherit"
+              :-webkit-flex-flow "inherit"}
+      :id "my-id"}
+     "text"]
+    (box/box :class "my-box" :attr {:id "my-id"} :child "text")))
+
+(deftest test-scroller
+  (are [expected actual] (= expected actual)
+    [:div
+     {:class "rc-scroller display-flex my-scroller"
+      :style {:flex "auto"
+              :-webkit-flex "auto"
+              :flex-flow "inherit"
+              :-webkit-flex-flow "inherit"
+              :overflow "auto"}
+      :id "my-id"}
+     "text"]
+    (box/scroller :class "my-scroller" :attr {:id "my-id"} :child "text")))
+
+(deftest test-border
+  (are [expected actual] (= expected actual)
+    [:div
+     {:class "rc-border display-flex my-border"
+      :style {:flex "none"
+              :-webkit-flex "none"
+              :flex-flow "inherit"
+              :-webkit-flex-flow "inherit"
+              :border "1px solid lightgrey"}
+      :id "my-id"}
+     "text"]
+    (box/border :class "my-border" :attr {:id "my-id"} :child "text")))
---
+++ test/re-com/validate_test.cljs
@@ -0,0 +1,66 @@
+(ns re-com-test.validate-test
+  (:require-macros [cemerick.cljs.test :refer (is are deftest with-test run-tests testing test-var)])
+  (:require [cemerick.cljs.test]
+            [reagent.core :as reagent]
+            [re-com.validate :as validate]))
+
+(deftest test-hash-map-with-name-keys
+  (let [obj1 {:name "obj1", :value 1}
+        obj2 {:name "obj2", :value 2}
+        dup  {:name "obj1", :value 3}]
+    (are [expected actual] (= expected actual)
+      {"obj1" obj1} (validate/hash-map-with-name-keys [obj1])
+      {"obj1" obj1, "obj2" obj2} (validate/hash-map-with-name-keys [obj1 obj2])
+      {"obj1" obj1, "obj2" obj2} (validate/hash-map-with-name-keys [obj1 obj2 obj1])
+      {"obj1" dup, "obj2" obj2} (validate/hash-map-with-name-keys [obj1 obj2 dup]))))
+
+(deftest test-extract-arg-data
+  (let [arg1 {:name "arg1", :required true, :validate-fn true}
+        arg2 {:name "arg2", :required false, :validate-fn true}
+        args [arg1
+              arg2
+              {:name "arg3", :required true, :validate-fn false}
+              {:name "arg4", :required false, :validate-fn false}]
+        arg-data (validate/extract-arg-data args)]
+    (is (= #{"arg1", "arg2", "arg3", "arg4"} (:arg-names arg-data)))
+    (is (= #{"arg1", "arg3"} (:required-args arg-data)))
+    (is (= {"arg1" arg1, "arg2" arg2} (:validated-args arg-data)))))
+
+(deftest test-arg-names-valid?
+  (are [expected actual] (= expected actual)
+    true (validate/arg-names-valid? #{:arg1} #{:arg1})
+    false (validate/arg-names-valid? #{:arg1} #{:arg2})
+    true (validate/arg-names-valid? #{:arg1 :arg2} #{:arg2})
+    false (validate/arg-names-valid? #{:arg1 :arg2} #{:arg1 :arg3})))
+
+(deftest test-required-args-passed?
+  (are [expected actual] (= expected actual)
+    true (validate/required-args-passed? #{:arg1} #{:arg1})
+    false (validate/required-args-passed? #{:arg1} #{:arg2})
+    false (validate/required-args-passed? #{:arg1 :arg2} #{:arg2})
+    false (validate/required-args-passed? #{:arg1 :arg2} #{:arg1 :arg3})
+    true (validate/required-args-passed? #{:arg1 :arg2} #{:arg1 :arg2})
+    true (validate/required-args-passed? #{:arg1 :arg2} #{:arg1 :arg2 :arg3})))
+
+(deftest test-extension-attribute?
+  (is (validate/extension-attribute? :data-attribute))
+  (is (not (validate/extension-attribute? :foo-attribute)))
+  (is (validate/extension-attribute? :aria-attribute))
+  (is (not (validate/extension-attribute? :dataattribute)))
+  (is (not (validate/extension-attribute? :ariaattribute))))
+
+(deftest test-string-or-atom?
+  (are [expected actual] (= expected actual)
+    true (validate/string-or-atom? "test")
+    false (validate/string-or-atom? 1)
+    true (validate/string-or-atom? (reagent/atom "test"))
+    false (validate/string-or-atom? (reagent/atom 1))))
+
+(deftest test-number-or-string?
+  (are [expected actual] (= expected actual)
+    true (validate/number-or-string? "test")
+    true (validate/number-or-string? 1)
+    true (validate/number-or-string? (reagent/atom "test"))
+    true (validate/number-or-string? (reagent/atom 1))
+    false (validate/number-or-string? [])
+    false (validate/number-or-string? (reagent/atom []))))
