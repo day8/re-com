bcd329842cc2c448918f6ba2eb4f5a59f4de4f9f
Isaac Johnston <isaac@superstruct.nz>
2019-08-26 01:02:19 +0300

    Migrate to shadow-cljs

---
+++ CHANGES.md
@@ -0,0 +1,6 @@
+## Unreleased
+
+#### Changes
+
+- Migrate to [shadow-cljs](https://shadow-cljs.github.io/docs/UsersGuide.html) and
+  [lein-shadow](https://gitlab.com/nikperic/lein-shadow)
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (require 'leiningen.core.eval)
 
-(def fig-port 3449)
+(def http-port 3449)
 
 ;; ---------------------------------------------------------------------------------------
 
@@ -9,38 +9,40 @@
   :url              "https://github.com/Day8/re-com.git"
   :license          {:name "MIT"}
 
-  :dependencies     [[org.clojure/clojure         "1.10.1"]
-                     [org.clojure/clojurescript   "1.10.520"]
-                     [reagent                     "0.8.1"]
-                     [org.clojure/core.async      "0.4.500"]
-                     [com.andrewmcveigh/cljs-time "0.5.2"]]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.520" :scope "provided"
+                  :exclusions [com.google.javascript/closure-compiler-unshaded
+                               org.clojure/google-closure-library]]
+                 [thheller/shadow-cljs "2.8.51" :scope "provided"]
+                 [reagent "0.8.1" :scope "provided"]
+                 [org.clojure/core.async "0.4.500"]
+                 [com.andrewmcveigh/cljs-time "0.5.2"]]
 
-  :profiles         {:dev       {:dependencies [[clj-stacktrace                  "0.2.8"]
-                                                [binaryage/devtools              "0.9.10"]
-                                                [binaryage/dirac                 "RELEASE"]]
-                                 :repl-options {:port 8230
-                                                :nrepl-middleware [dirac.nrepl/middleware]
-                                                :init (do (require 'dirac.agent)
-                                                          (dirac.agent/boot!))}
-                                 :plugins      [[lein-cljsbuild                  "1.1.7"]
-                                                [lein-figwheel                   "0.5.18"]
-                                                [lein-shell                      "0.5.0"]
-                                                [org.clojure/data.json           "0.2.6"]
-                                                [lein-ancient                    "0.6.15"]]}
-                     :demo      {:dependencies [[alandipert/storage-atom         "2.0.1"]
-                                                [com.cognitect/transit-cljs      "0.8.239"] ;; Overrides version in storage-atom which prevents compiler warnings about uuid? and boolean? being replaced
-                                                [clj-commons/secretary           "1.2.4"]]}
-                     :dev-cider {:figwheel {:nrepl-port       7777
-                                            :nrepl-middleware ["cider.nrepl/cider-middleware"
-                                                               "cemerick.piggieback/wrap-cljs-repl"]}
-                                 :dependencies [[com.cemerick/piggieback "0.2.2"]
-                                                #_[figwheel-sidecar "0.5.12"]]}
-                     :dev-run   {:clean-targets ^{:protect false} ["run/resources/public/compiled_dev"]}
-                     :prod-run  {:clean-targets ^{:protect false} ["run/resources/public/compiled_prod"]}
-                     :dev-test  {:clean-targets ^{:protect false} ["run/test/compiled"]}}
+  :plugins [[lein-shadow "0.1.5"]]
 
+  :profiles {:dev      {:dependencies [[clj-stacktrace "0.2.8"]
+                                       [binaryage/devtools "0.9.10"]]
+                        :plugins      [[lein-shell "0.5.0"]
+                                       [org.clojure/data.json "0.2.6"]
+                                       [lein-ancient "0.6.15"]]}
+             :demo     {:dependencies [[alandipert/storage-atom "2.0.1"]
+                                       [com.cognitect/transit-cljs "0.8.256"] ;; Overrides version in storage-atom which prevents compiler warnings about uuid? and boolean? being replaced
+                                       [clj-commons/secretary "1.2.4"]]}
+             :dev-run  {:source-paths  ["dev-src"]}
+             :prod-run {}
+             :dev-test {:source-paths  ["dev-src"]}}
+
+  :source-paths    ["src"]
   :test-paths      ["test"]
   :resource-paths  ["run/resources"]
+
+  :clean-targets ^{:protect false} [".shadow-cljs"
+                                    "shadow-cljs.edn"
+                                    "package.json"
+                                    "package-lock.json"
+                                    "run/resources/public/compiled_dev"
+                                    "run/resources/public/compiled_prod"
+                                    "run/resources/public/compiled_test"]
 
   :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
                         ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
@@ -48,40 +50,25 @@
   ;; Exclude the demo and compiled files from the output of either 'lein jar' or 'lein install'
   :jar-exclusions   [#"(?:^|\/)re_demo\/" #"(?:^|\/)demo\/" #"(?:^|\/)compiled.*\/" #"html$"]
 
-  :cljsbuild {:builds [{:id           "demo"
-                        :source-paths ["dev-src" "src"]
-                        :figwheel     {:on-jsload     "re-demo.core/mount-demo"}
-                        :compiler     {:preloads        [devtools.preload day8.app.dev-preload]
-                                       :external-config {:devtools/config {:features-to-install [:formatters :hints]}}
-                                       :output-to       "run/resources/public/compiled_dev/demo.js"
-                                       :output-dir      "run/resources/public/compiled_dev/demo"
-                                       :main            "re-demo.core"
-                                       :asset-path      "compiled_dev/demo"
-                                       :source-map      true
-                                       :optimizations   :none
-                                       :pretty-print    true}}
-                       {:id           "prod"
-                        :source-paths ["src"]
-                        :compiler     {:output-to       "run/resources/public/compiled_prod/demo.js"
-                                       :output-dir      "run/resources/public/compiled_prod/demo"
-                                       :closure-defines {"goog.DEBUG" false}
-                                       :optimizations   :advanced
-                                       :pretty-print    false
-                                       :pseudo-names    false}}
-                       {:id           "test"
-                        :source-paths ["dev-src" "src/re_com" "test"]
-                        :compiler     {:preloads        [devtools.preload day8.app.dev-preload]
-                                       :external-config {:devtools/config {:features-to-install [:formatters :hints]}}
-                                       :output-to       "run/test/compiled/test.js"
-                                       :output-dir      "run/test/compiled/test"
-                                       :source-map      true
-                                       :optimizations   :none
-                                       :pretty-print    true}}]
-              :test-commands   {}} ;; figwheel 0.5.2 required this for some reason
+  :shadow-cljs {:nrepl  {:port 7777}
 
-  :figwheel {:css-dirs    ["run/resources/public/assets/css"]
-             :server-port ~fig-port
-             :repl        false}
+                :builds {:demo         {:target   :browser
+                                        :modules  {:demo {:init-fn  re-demo.core/mount-demo
+                                                          :preloads [day8.app.dev-preload]}}
+                                        :dev      {:asset-path       "/compiled_dev/demo"
+                                                   :output-dir       "run/resources/public/compiled_dev/demo"
+                                                   :compiler-options {:external-config {:devtools/config {:features-to-install [:formatters :hints]}}}}
+                                        :release  {:output-dir "run/resources/public/compiled_prod/demo"}
+                                        :devtools {:http-port ~http-port
+                                                   :http-root "run/resources/public"}}
+
+                         :browser-test {:target           :browser-test
+                                        :ns-regexp        "-test$"
+                                        :test-dir         "run/resources/public/compiled_test/demo"
+                                        :compiler-options {:external-config {:devtools/config {:features-to-install [:formatters :hints]}}}
+                                        :devtools         {:http-port 8021
+                                                           :http-root "run/resources/public/compiled_test/demo"
+                                                           :preloads  [day8.app.dev-preload]}}}}
 
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
@@ -98,30 +85,20 @@
 
   :aliases          {;; *** DEV ***
 
-                     "dev-once"   ["with-profile" "+dev-run,+demo" "do"
-                                   ["clean"]
-                                   ["cljsbuild" "once" "demo"]
-                                   ["shell" "open" "run/resources/public/index_dev.html"]]
-
                      "dev-auto"   ["with-profile" "+dev-run,+demo" "do"
                                    ["clean"]
-                                   ~["shell" "open" (str "http://localhost:" fig-port "/index_dev.html")]   ;; NOTE: run will initially fail, refresh browser once build complete
-                                   ["figwheel" "demo"]]
+                                   ~["shell" "open" (str "http://localhost:" http-port "/index_dev.html")]   ;; NOTE: run will initially fail, refresh browser once build complete
+                                   ["shadow" "watch" "demo"]]
 
                      ;; *** PROD ***
 
                      "prod-once"  ["with-profile" "+prod-run,+demo,-dev" "do"
                                    ["clean"]
-                                   ["cljsbuild" "once" "prod"]
-                                   ["shell" "open" "run/resources/public/index_prod.html"]]
-
-                     "prod-auto"  ["with-profile" "+prod-run,+demo,-dev" "do"
-                                   ["prod-once"]
-                                   ["cljsbuild" "auto" "prod"]]
+                                   ["shadow" "release" "demo"]]
 
                      "deploy-aws" ["with-profile" "+prod-run,+demo,-dev" "do"
                                    ["clean"]
-                                   ["cljsbuild" "once" "prod"]
+                                   ["shadow" "release" "demo"]
                                    ~["shell" "aws" "s3" "--profile=day8" "sync" "run/resources/public" "s3://re-demo/" "--acl" "public-read" "--cache-control" "max-age=2592000,public"]]
 
                      ;; *** TEST ***
@@ -129,16 +106,11 @@
                      "test" ["do"
                              ["with-profile" "+dev-test" "do"
                               ["clean"]
-                              ["cljsbuild" "once" "test"]]
+                              ["shadow" "compile" "browser-test"]]
                              ["with-profile" "+prod-run,+demo,-dev" "do"
                               ["clean"]
-                              ["cljsbuild" "once" "prod"]]]
-                     "test-once"  ["with-profile" "+dev-test" "do"
-                                   ["clean"]
-                                   ["cljsbuild" "once" "test"]
-                                   ["shell" "open" "run/test/test.html"]]
+                              ["shadow" "release" "demo"]]]
 
                      "test-auto"  ["with-profile" "+dev-test" "do"
-                                   ["test-once"]
-                                   ["cljsbuild" "auto" "test"]]})
+                                    ["shadow" "watch" "browser-test"]]})
 
