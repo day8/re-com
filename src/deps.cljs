dbb649d0bda27771f4fa69915b58a351f01dc388
Isaac Johnston <isaac@superstruct.nz>
2019-12-17 15:22:04 +0700

    Upgrade shadow-cljs to 2.8.83

--- src/deps.cljs
+++ src/deps.cljs
@@ -3,7 +3,7 @@
                 "react"              "16.8.6"
                 "react-dom"          "16.8.6"}
 
- :npm-dev-deps {"shadow-cljs"           "2.8.81"
+ :npm-dev-deps {"shadow-cljs"           "2.8.83"
                 "karma"                 "4.4.1"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
