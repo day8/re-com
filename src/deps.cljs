fe9b26e32c6fdb7679f452ad1887846352bbf585
Isaac Johnston <isaac@superstruct.nz>
2020-09-23 17:39:50 +1200

    Upgrade lein-shadow to 0.3.0

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,5 +1,5 @@
-{:npm-dev-deps {"shadow-cljs"           "2.10.19"
-                "karma"                 "5.1.0"
+{:npm-dev-deps {"shadow-cljs"           "2.11.4"
+                "karma"                 "5.2.2"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
                 "karma-junit-reporter"  "2.0.1"}}
