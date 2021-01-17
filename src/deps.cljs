cbb2a191579f4d0b87fa443bd84532c4b9693a76
Isaac Johnston <isaac@superstruct.nz>
2021-01-18 10:19:21 +1300

    Upgrade shadow-cljs to 2.11.14
    Hoping to fix #226

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,4 +1,4 @@
-{:npm-dev-deps {"shadow-cljs"           "2.11.4"
+{:npm-dev-deps {"shadow-cljs"           "2.11.14"
                 "karma"                 "5.2.2"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
