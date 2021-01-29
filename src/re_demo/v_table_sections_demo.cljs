f42a585795f54f492b83499547ee43b209ab570c
Gregg8 <gregg.ramsey@day8.com.au>
2021-01-29 22:37:19 +1100

    Improved the small v-table demo

--- src/re_demo/v_table_sections_demo.cljs
+++ src/re_demo/v_table_sections_demo.cljs
@@ -1,34 +1,43 @@
 (ns re-demo.v-table-sections-demo
-  (:require [re-com.core   :refer [h-box gap v-box box v-table hyperlink-href p label]]
-            [re-demo.utils :refer [panel-title title2 title3 args-table github-hyperlink status-text]]
-            [re-com.validate    :refer [vector-atom? vector-or-atom? map-atom? parts?]]
-            [reagent.core  :as reagent]))
+  (:require [re-com.core     :refer [h-box gap v-box box v-table hyperlink-href p label]]
+            [re-demo.utils   :refer [panel-title title2 title3 args-table github-hyperlink status-text]]
+            [re-com.validate :refer [vector-atom? vector-or-atom? map-atom? parts?]]
+            [re-com.util     :refer [px]]
+            [reagent.core    :as reagent]))
 
 
-(def big-font 18)
-(def small-font 10)
+(def big-font "18px")
+(def small-font "10px")
 
 (defn render-two-lines
-  [name section background]
+  [name section background height]
   [v-box
-   ; :height "40px"
-   :size  "1 0 auto"
-   :style {:color "white" :background-color background} 
-   :align :center 
+   :height   (px height)
+   :style    {:color            "white"
+              :background-color background}
+   :align    :center
    :children [[label :label section :style {:font-size big-font}]
               [label :label name :style {:font-size small-font}]]])
 
 
-(defn sections-demo 
+(defn sections-demo
   []
   (let [light-blue                "#DBEFF9"
         medium-blue               "#5B9BD5"
         blue                      "#0F6FC6"
         num_rows                  6
         dummy-rows                (reagent/atom (mapv #(assoc nil :id %1) (range num_rows))) ;; TODO: Changed to atom for testing validation-fn, can change back when we successfully allow atom OR value
-        row-height                40
+        row-height                25
+        col-header-footer-height  40
         width-of-main-row-content 250
-        header-footer-style       {:style {:flex "1 0 auto" :color "white" :background-color medium-blue}}]
+        header-footer-style       {:style {:height           (px row-height)
+                                           :text-align       "center"
+                                           :color            "white"
+                                           :background-color medium-blue}}
+        row-style                 {:style {:width            (px width-of-main-row-content)
+                                           :height           (px row-height)
+                                           :text-align       "center"
+                                           :background-color light-blue}}]
     (fn []
       [v-box
        :gap      "10px"
@@ -36,29 +45,29 @@
                   [:p {:style {:width "450px"}} "There are nine sections in a v-table, of which only #5 is mandatory. This table has 6 rows of data. "]
 
                   [v-table
-                   :model              dummy-rows
-                   :row-height         row-height
-                   :row-content-width  width-of-main-row-content
-
-                   ;; section 2
-                   :row-header-renderer    (fn [row-index, row] [:span header-footer-style (str row-index (when (= row-index 2) " ") (when (= row-index 3) " "))])
-                   :row-footer-renderer    (fn [row-index, row] [:span header-footer-style (str row-index)])
+                   :model                  dummy-rows
+                   :row-height             row-height
+                   :row-content-width      width-of-main-row-content
 
                    ;; column header - section 4
-                   :column-header-height   40
-                   :column-header-renderer (fn [] [render-two-lines "column headers" #_"" "4" medium-blue])
+                   :column-header-height   col-header-footer-height
+                   :column-header-renderer (fn [] [render-two-lines "column headers" "4" medium-blue col-header-footer-height])
 
-                   ;; column footer - section 5
-                   :column-footer-height   40
-                   :column-footer-renderer (fn [] [render-two-lines "column footers" "6" medium-blue])
+                   ;; row header, rows, row footer - sections 2, 5, 8
+                   :row-header-renderer    (fn [row-index row] [:span header-footer-style (str "hdr " row-index)])
+                   :row-renderer           (fn [row-index row] [:span row-style           (str "row " row-index)])
+                   :row-footer-renderer    (fn [row-index row] [:span header-footer-style (str "ftr " row-index)])
+
+                   ;; column footer - section 6
+                   :column-footer-height   col-header-footer-height
+                   :column-footer-renderer (fn [] [render-two-lines "column footers" "6" medium-blue col-header-footer-height])
 
                    ;; corners
-                   :top-left-renderer     (fn [] [render-two-lines "top left"     "1" blue])
-                   :bottom-left-renderer  (fn [] [render-two-lines "bottom left"  "3" blue])
-                   :bottom-right-renderer (fn [] [render-two-lines "bottom right" "9" blue])
-                   :top-right-renderer    (fn [] [render-two-lines "top right"    "7" blue])
-
-                   :row-renderer           (fn [row-index, row] [:div  {:style { :width width-of-main-row-content :background-color light-blue}} " xxx"])]]])))
+                   :top-left-renderer     (fn [] [render-two-lines "top left"     "1" blue col-header-footer-height])
+                   :bottom-left-renderer  (fn [] [render-two-lines "bottom left"  "3" blue col-header-footer-height])
+                   :top-right-renderer    (fn [] [render-two-lines "top right"    "7" blue col-header-footer-height])
+                   :bottom-right-renderer (fn [] [render-two-lines "bottom right" "9" blue col-header-footer-height])
+                   ]]])))
 
 
 ;; MT's Notes: 
