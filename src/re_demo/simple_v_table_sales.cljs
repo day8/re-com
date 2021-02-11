23e347e9d3225759090c3c51f876120bd22938f2
Gregg8 <gregg.ramsey@day8.com.au>
2021-02-11 22:20:37 +1100

    Added feature to fill the table with either a random numbers of rows or specify between 100 and 100,000 rows to stress test the virtual capability

--- src/re_demo/simple_v_table_sales.cljs
+++ src/re_demo/simple_v_table_sales.cljs
@@ -4,7 +4,7 @@
     [goog.string           :as gstring]
     [goog.string.format]
     [reagent.core          :as reagent]
-    [re-com.core           :refer [label p slider hyperlink-href h-box v-box gap simple-v-table checkbox]]
+    [re-com.core           :refer [label p slider hyperlink-href h-box v-box gap simple-v-table checkbox button]]
     [re-demo.utils         :refer [title3]]
     [re-com.util           :refer [px]]))
 
@@ -16,8 +16,9 @@
    "Sylvia" "Abbigale" "Allissa" "Alona" "Asira" "Aurora-Leigh" "Brea" "Chiarra" "Elouisa" "Emme-Leigh" "Frea" "Geena"
    "Hanifa"])
 
-;; Generate 100 random rows.
-(def sales-rows
+;; Generate between 0-100 random rows.
+(defn generate-sales-rows
+  [& [row-count]]
   (mapv
     (fn [n]
       (let [person (rand-nth names)]
@@ -28,7 +29,7 @@
          :email  (str (string/lower-case person) "@" (rand-nth ["widget.org" "example.com" "deck.com" "conversation.com" "response.com"]))
          :sales  (gstring/format "%.2f" (+ (* (rand) (- 10000 1000)) 1000))
          :units  (+ (* (rand-int 10) (- 1000 100)) 100)}))
-    (range 100)))
+    (range row-count)))
 
 (defn store-icon
   []
@@ -42,7 +43,7 @@
 
 (defn demo
   []
-  (let [sales-rows          (reagent/atom sales-rows)
+  (let [sales-rows          (reagent/atom (generate-sales-rows 100))
         max-rows?           (reagent/atom true)
         max-rows            (reagent/atom 8)
         max-width?          (reagent/atom false)
@@ -54,6 +55,8 @@
         parent-width        (reagent/atom 600)
         parent-height?      (reagent/atom false)
         parent-height       (reagent/atom 600)
+        how-many?           (reagent/atom false)
+        how-many            (reagent/atom 100)
         spacing             (px 12)
         spacing7           (px 7)
         email-row-label-fn  (fn [row] [hyperlink-href :label (:email row) :href (str "mailto:" (:email row))])
@@ -113,8 +116,17 @@
                                            :align    :center
                                            :children [[checkbox
                                                        :model     max-rows?
-                                                       :on-change #(reset! max-rows? %)]
-                                                      [label :label [:code ":max-rows"]]
+                                                       :on-change (fn [new-max-rows?]
+                                                                    (let [rc (count @sales-rows)]
+                                                                      (if new-max-rows?
+                                                                        (reset! max-rows? new-max-rows?)
+                                                                        (if (or (<= rc 100) @parent-height?)
+                                                                          (reset! max-rows? new-max-rows?)))))]
+                                                      [label :label [:code
+                                                                     (when (and (> (count @sales-rows) 100) (not @parent-height?))
+                                                                       {:title "Can't uncheck. Table would be too large.\nMust check 'parent height' before you can uncheck this"
+                                                                        :style {:color "red"}})
+                                                                     ":max-rows"]]
                                                       (if @max-rows?
                                                         [:<>
                                                          [slider
@@ -127,10 +139,10 @@
                                                          [gap :size spacing]
                                                          [label :label @max-rows]
                                                          [gap :size spacing7]
-                                                         [label :label  "(this demo has 100 rows of data)"]]
+                                                         [label :label (str "(this demo has " (count @sales-rows) " rows of data)")]]
                                                         [label
                                                          :style {:background-color parent-color}
-                                                         :label (str "when unset, the table's height is the parent's explicit height, or if that's unset, the number of rows of data (100)")])]]
+                                                         :label (str "when unset, the table's height is the parent's explicit height, or if that's unset, the number of rows of data (" (count @sales-rows) ")")])]]
                                           [gap :size "0px"]
                                           [v-box
                                            :class    "parent-for-simple-v-table"
@@ -164,8 +176,17 @@
                                                        :align    :center
                                                        :children [[checkbox
                                                                    :model     parent-height?
-                                                                   :on-change #(reset! parent-height? %)]
-                                                                  [label :label [:code "parent height"]]
+                                                                   :on-change (fn [new-parent-height?]
+                                                                                (let [rc (count @sales-rows)]
+                                                                                  (if new-parent-height?
+                                                                                    (reset! parent-height? new-parent-height?)
+                                                                                    (if (or (<= rc 100) @max-rows?)
+                                                                                      (reset! parent-height? new-parent-height?)))))]
+                                                                  [label :label [:code
+                                                                                 (when (and (> (count @sales-rows) 100) (not @max-rows?))
+                                                                                   {:title "Can't uncheck. Table would be too large.\nMust check :max-rows before you can uncheck this"
+                                                                                    :style {:color "red"}})
+                                                                                 "parent height"]]
                                                                   (if @parent-height?
                                                                     [:<>
                                                                      [slider
@@ -177,7 +198,37 @@
                                                                       :width     (px 200)]
                                                                      [gap :size spacing7]
                                                                      [label :label (px @parent-height)]]
-                                                                    [label :label "unset: grows to table's natural extent (100 rows of data) or the :max-rows override"])]]
+                                                                    [label :label (str "unset: grows to table's natural extent (" (count @sales-rows) " rows of data) or the :max-rows override")])]]
+                                                      [h-box
+                                                       :gap      spacing7
+                                                       :align    :center
+                                                       :children [[label
+                                                                   :width "135px"
+                                                                   :label [:span "Rows in table: " [:span.bold (count @sales-rows)]]]
+                                                                  [button
+                                                                   :label    (str "Generate " (if @how-many? @how-many "random") " rows")
+                                                                   :style    {:width   "170px"
+                                                                              :height  "24px"
+                                                                              :padding "0px"}
+                                                                   :on-click #(do (when (and @how-many? (> @how-many 100) (not @max-rows?) (not @parent-height?))
+                                                                                    (reset! parent-height? true))
+                                                                                  (reset! sales-rows (generate-sales-rows (if @how-many? @how-many (+ 5 (rand 95))))))]
+                                                                  [checkbox
+                                                                   :model     how-many?
+                                                                   :on-change #(reset! how-many? %)]
+                                                                  [label :label [:code "how many?"]]
+                                                                  (if @how-many?
+                                                                    [:<>
+                                                                     [slider
+                                                                      :model     how-many
+                                                                      :on-change #(reset! how-many %)
+                                                                      :min       100
+                                                                      :max       100000
+                                                                      :step      100
+                                                                      :width     (px 200)]
+                                                                     [gap :size spacing7]
+                                                                     [label :label @how-many]]
+                                                                    [label :label "random number between 5 and 99"])]]
 
                                                       ;; vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
                                                       ;; The simple-v-table demo starts here
@@ -194,11 +245,11 @@
                                                                                                            (> 5000 sales 2000) "#FFDC00"
                                                                                                            (> 7500 sales 5000) "#01FF70"
                                                                                                            (> sales 7500)      "#2ECC40")}))
-                                                       :columns                   [{:id :id     :header-label "Code"   :row-label-fn :id                   :width 60  :align "left"  :vertical-align "middle"}
-                                                                                   {:id :region :header-label "Region" :row-label-fn :region               :width 100 :align "left"  :vertical-align "middle"}
-                                                                                   {:id :name   :header-label "Name"   :row-label-fn :person               :width 100 :align "left"  :vertical-align "middle" :sort-by {}}
-                                                                                   {:id :email  :header-label "Email"  :row-label-fn email-row-label-fn    :width 200 :align "left"  :vertical-align "middle"}
-                                                                                   {:id :method :header-label "Method" :row-label-fn method-row-label-fn   :width 100 :align "right" :vertical-align "middle"}
-                                                                                   {:id :sales  :header-label "Sales"  :row-label-fn #(str "$" (:sales %)) :width 100 :align "right" :vertical-align "middle" :sort-by {:key-fn :sales}}
-                                                                                   {:id :units  :header-label "Units"  :row-label-fn :units                :width 100 :align "right" :vertical-align "middle" :sort-by true}]
+                                                       :columns                   [{:id :id     :header-label "Code"   :row-label-fn :id                   :width 60  :align "center" :vertical-align "middle"}
+                                                                                   {:id :region :header-label "Region" :row-label-fn :region               :width 100 :align "left"   :vertical-align "middle"}
+                                                                                   {:id :name   :header-label "Name"   :row-label-fn :person               :width 100 :align "left"   :vertical-align "middle" :sort-by {}}
+                                                                                   {:id :email  :header-label "Email"  :row-label-fn email-row-label-fn    :width 200 :align "left"   :vertical-align "middle"}
+                                                                                   {:id :method :header-label "Method" :row-label-fn method-row-label-fn   :width 100 :align "center" :vertical-align "middle"}
+                                                                                   {:id :sales  :header-label "Sales"  :row-label-fn #(str "$" (:sales %)) :width 100 :align "right"  :vertical-align "middle" :sort-by {:key-fn :sales}}
+                                                                                   {:id :units  :header-label "Units"  :row-label-fn :units                :width 100 :align "right"  :vertical-align "middle" :sort-by true}]
                                                        :model                     sales-rows]]]]]]]]])))
