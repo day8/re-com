2385f3d1c04ffe6984b1e00f05d335486e03cb86
Mike Thompson <mike.thompson@day8.com.au>
2021-02-20 12:01:58 +1100

    Update v_table_renderers.cljs

--- src/re_demo/v_table_renderers.cljs
+++ src/re_demo/v_table_renderers.cljs
@@ -37,49 +37,25 @@
       [v-table
        :model                   dummy-rows
 
-       ;; ===== Column header/footer (sections 4,6)
+       ;; Data Rows (section 5)
+       :row-renderer            (fn [_row_index, _row] [box-with-border {:name ":row-renderer" :background light-blue :height row-height :width width-of-main-row-content}])
+       :row-content-width       width-of-main-row-content
+       :row-height              row-height
+       :max-row-viewport-height (- total-row-height row-height)      ;; force a vertical scrollbar
+
+       ;; row header/footer (sections 2,8)
+       :row-header-renderer     (fn [_row-index, _row] [box-with-border {:name ":row-header-renderer " :background green :height unit-31 :width unit-121}])
+       :row-footer-renderer     (fn [_row-index, _row] [box-with-border {:name ":row-footer-renderer"  :background green :height unit-31 :width unit-121}])
+
+       ;; column header/footer (sections 4,6)
        :column-header-renderer  (fn [] [box-with-border {:name ":column-header-renderer" :background gold :height unit-50 :width width-of-main-row-content}])
        :column-header-height    unit-50
        :column-footer-renderer  (fn [] [box-with-border {:name ":column-footer-renderer" :background "#d8d460" :height unit-50 :width width-of-main-row-content}])
        :column-footer-height    unit-50
 
-       ;; ===== Row header/footer (sections 2,8)
-       :row-header-renderer     (fn [_row-index, _row] [box-with-border {:name ":row-header-renderer " :background green :height unit-31 :width unit-121}])
-       :row-footer-renderer     (fn [_row-index, _row] [box-with-border {:name ":row-footer-renderer"  :background green :height unit-31 :width unit-121}])
-
-       ;; ===== Rows (section 5)
-       :row-renderer            (fn [_row_index, _row] [box-with-border {:name ":row-renderer" :background light-blue :height row-height :width width-of-main-row-content}])
-       :row-content-width       width-of-main-row-content
-       :row-height              row-height
-       :max-row-viewport-height (- total-row-height row-height)    ;; force a vertical scrollbar
-
-       ;; ===== Corners (sections 1,3,7,9)
+       ;; 4 corners (sections 1,3,7,9)
        :top-left-renderer       (fn [] [box-with-border {:name ":top-left-renderer"     :background blue  :height unit-50 :width unit-121}])
        :bottom-left-renderer    (fn [] [box-with-border {:name ":bottom-left-renderer"  :background blue  :height unit-50 :width unit-121}])
        :top-right-renderer      (fn [] [box-with-border {:name ":top-right-renderer"    :background blue  :height unit-50 :width unit-121}])
        :bottom-right-renderer   (fn [] [box-with-border {:name ":bottom-right-renderer" :background blue  :height unit-50 :width unit-121}])])))
-
-
-;; MT's Notes: 
-;; 
-;; On section width:
-;;   - the width of left sections 1,2,3 is determined by the widest hiccup returned by the 3 renderers for these sections. 
-;;   - the width of center  sections 4,5,6 is determined by `:row-content-width`
-;;   - the width of left sections 7,8,9 is determined by by the widest hiccup returned by the 3 renderers for these sections.
-;; 
-;; the viewport width for 4,5,6 is determined by the widest hiccup returned by renderers.  Once I put in an `h-box` it expanded out. When i only had `div` the viewport collapsed to the size of the content.
-;; puzzled about column headings XXX
-;; 
-;; For `:row-viewport-width` the docs say if not specified will take up all available space but this is not 
-;; correct. 
-;; 
-;; I have to provide `:column-header-height`. Could the height of top sections 1, 4, 7 should provide the height. 
-;; 
-;; Mention in docs that you are likely to use h-box and v-box in renderers.
-;; 
-;; Discuss with Gregg and Isaac:
-;;   - the idea of variable row heights. 
-;;   - performance: we have to reduce the amount of inline styles
-;;   - How do I create "CSS classes" in a namespace
-
 
