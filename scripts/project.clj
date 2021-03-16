19966683dc527463a18c4eeeedc39ce029cb3b5f
Ike Mawira <ike@superstruct.nz>
2021-03-16 17:14:45 +0300

    Update project.clj description.

--- scripts/project.clj
+++ scripts/project.clj
@@ -1,5 +1,5 @@
 (defproject scripts "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Script to modify sources with :src annotations."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
