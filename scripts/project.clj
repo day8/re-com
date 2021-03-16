00e4e8877f957dc8c2a1bf45ec297557e40febf5
Ike Mawira <ike@superstruct.nz>
2021-03-16 17:10:44 +0300

    Add project.clj

---
+++ scripts/project.clj
@@ -0,0 +1,9 @@
+(defproject scripts "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :main scripts.core
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [rewrite-clj "1.0.579-alpha"]]
+  :repl-options {:init-ns scripts.core})
