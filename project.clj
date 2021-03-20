8d867c13fd973a043bd8974b025a02b7fe596e35
Isaac Johnston <isaac@superstruct.nz>
2021-03-21 11:45:06 +1300

    Upgrade Clojure to 1.10.3

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :url              "https://github.com/day8/re-com.git"
   :license          {:name "MIT"}
 
-  :dependencies [[org.clojure/clojure         "1.10.2"   :scope "provided"]
+  :dependencies [[org.clojure/clojure         "1.10.3"   :scope "provided"]
                  [org.clojure/clojurescript   "1.10.773" :scope "provided"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library
