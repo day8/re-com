31c30b0002bf147745138241fbc809146294f90d
Isaac Johnston <isaac@superstruct.nz>
2021-07-21 15:03:32 +1200

    Upgrade karma to 6.3.4

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,7 +1,7 @@
 {:npm-dev-deps {"react"                 "17.0.2"
                 "react-dom"             "17.0.2"
                 "shadow-cljs"           "2.15.2"
-                "karma"                 "6.2.0"
+                "karma"                 "6.3.4"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
                 "karma-junit-reporter"  "2.0.1"}}
