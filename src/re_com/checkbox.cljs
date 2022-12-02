9dc2047ab9259e8605c2ae79f4d1ffe635bf283e
bnmcgn@gmail.com <bnmcgn@gmail.com>
2022-12-02 08:11:01 -0800

    Css split out

--- src/re_com/checkbox.cljs
+++ src/re_com/checkbox.cljs
@@ -5,7 +5,7 @@
   (:require
     [re-com.debug     :refer [->attr]]
     [re-com.config    :refer [include-args-desc?]]
-    [re-com.util      :refer [deref-or-value px]]
+    [re-com.util      :refer [deref-or-value px add-map-to-hiccup-call merge-css flatten-attr]]
     [re-com.popover   :refer [popover-tooltip]]
     [re-com.box       :refer [h-box v-box box gap line flex-child-style align-style]]
     [re-com.validate  :refer [input-status-type? input-status-types-list regex? string-or-hiccup? css-style? html-attr? parts?
@@ -25,6 +25,16 @@
 (def checkbox-parts
   (when include-args-desc?
     (-> (map :name checkbox-parts-desc) set)))
+
+(def checkbox-css-spec
+  {:wrapper {:class ["rc-checkbox-wrapper" "noselect"]}
+   :main {:class ["rc-checkbox"]
+          :style (merge (flex-child-style "none")
+                        {:cursor "default"})}
+   :label {:class ["rc-checkbox-label"]
+           :style (merge (flex-child-style "none")
+                         {:padding-left "8px"
+                          :cursor "default"})}})
 
 (def checkbox-args-desc
   (when include-args-desc?
@@ -51,31 +61,27 @@
           model       (deref-or-value model)
           disabled?   (deref-or-value disabled?)
           callback-fn #(when (and on-change (not disabled?))
-                        (on-change (not model)))]  ;; call on-change with either true or false
-      [h-box
-       :src      src
-       :debug-as (or debug-as (reflect-current-component))
-       :class    (str "noselect rc-checkbox-wrapper " (get-in parts [:wrapper :class]))
-       :style    (get-in parts [:wrapper :style])
-       :attr     (get-in parts [:wrapper :attr])
-       :align    :start
-       :children [[:input
-                   (merge
-                     {:class     (str "rc-checkbox " class)
-                      :type      "checkbox"
-                      :style     (merge (flex-child-style "none")
-                                        {:cursor cursor}
-                                        style)
+                         (on-change (not model)))  ;; call on-change with either true or false
+          cmerger (merge-css checkbox-css-spec args)]
+      (add-map-to-hiccup-call
+       (cmerger :wrapper)
+       [h-box
+        :src      src
+        :debug-as (or debug-as (reflect-current-component))
+        :align    :start
+        :children [[:input
+                    (merge
+                     (flatten-attr (cmerger :main))
+                     {:type      "checkbox"
                       :disabled  disabled?
                       :checked   (boolean model)
                       :on-change (handler-fn (callback-fn))}
                      attr)]
-                  (when label
-                    [:span
-                     {:class    (str "rc-checkbox-label " label-class)
-                      :style    (merge (flex-child-style "none")
-                                       {:padding-left "8px"
-                                        :cursor       cursor}
-                                       label-style)
-                      :on-click (handler-fn (callback-fn))}
-                     label])]])))
+                   (when label
+                     [:span
+                      (flatten-attr
+                       (cmerger :label
+                                {:class label-class
+                                 :style label-style
+                                 :attr {:on-click (handler-fn (callback-fn))}}))
+                      label])]]))))
