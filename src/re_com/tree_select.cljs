bf7c0eb24773309e1e683ed8d4e1d5a30219ff28
Kimo Knowles <kimo.knowles@day8.com.au>
2023-12-17 11:00:57 +0100

    [wip] Add tree-select

--- src/re_com/tree_select.cljs
+++ src/re_com/tree_select.cljs
@@ -1,44 +1,60 @@
 (ns re-com.tree-select
-  (:require-macros
-   [re-com.core     :refer [handler-fn at]]
-   [re-com.validate :refer [validate-args-macro]])
   (:require
-   [clojure.string :as str]
+   [clojure.set :as set]
    [reagent.core          :as r]
-   [re-com.multi-select   :refer [multi-select]]
-   [re-com.config         :refer [include-args-desc?]]
    [re-com.util           :refer [deref-or-value]]
-   [re-com.validate       :as validate :refer [parts? string-or-hiccup?]]
-   [re-com.box            :refer [h-box v-box gap box]]
-   [re-com.text           :as text]
-   [re-com.selection-list :as    selection-list]))
+   [re-com.box            :refer [h-box v-box box]]))
 
-(defn choice [{:keys [label checked? toggle! level showing?]}]
-  (when showing?
-    [:div {:style {:margin-left (str (inc level) "rem")}}
-     [:span {:on-click toggle!
-             :style {:cursor "pointer"}}
-      (if checked? "☑" "☐")]
-     " "
-     [:i (when showing? label)]]))
+(def tree-select-parts-desc [])
 
-(defn group [{:keys [label checked? toggle! hide-show! level showing? open?]}]
+(def tree-select-args-desc [])
+
+(defn choice [{:keys [label checked? toggle! level showing? width style disabled?]}]
   (when showing?
-    [:div {:style {:margin-left (str (dec level) "rem")}}
-     [:span {:on-click hide-show!
-             :style {:cursor "pointer"}}
-      (if open? "⯆" "⯈")]
-     " "
-     [:span {:on-click toggle!
-             :style {:cursor "pointer"}}
-      (case checked? :all "☑" :some "◽" "☐")]
-     " "
-     [:strong label]]))
+    [h-box
+     :children
+     [[box
+       :attr {:on-click (when-not disabled? toggle!)}
+       :style {:cursor (if disabled? "default" "pointer")}
+       :child
+       (if checked? "☑" "☐")]
+      " "
+      label]]))
+
+(defn group [{:keys [label checked? toggle! hide-show! level showing? open? disabled?]}]
+  (when showing?
+    [h-box :class "chosen-container chosen-container-single chosen-container-active"
+     :style {:margin-left (str (dec level) "rem")}
+     :children
+     [[box
+       :attr {:on-click (when-not disabled? hide-show!)}
+       :style {:cursor "pointer"}
+       :child
+       (if open? "⯆" "⯈")]
+      " "
+      #_[checkbox :src (at)
+         :model checked?
+         :on-change toggle!
+         :label label
+         :disabled? disabled?
+         :style {}]
+      [box
+       :attr {:on-click (when-not disabled? toggle!)}
+       :style {:cursor "default"}
+       :child
+       (case checked? :all "☑" :some "◽" "☐")]
+      " "
+      [:span
+       {:style {:overflow "none"
+                :word-wrap "break-word"}}
+       label]]]))
 
 (def group? (comp #{:group} :type))
 
+(defn as-v [x] (when (some? x) (if (vector? x) x [x])))
+
 (defn ancestor-paths [path]
-  (->> path (iterate butlast) (take-while identity) (map vec)))
+  (some->> path as-v (iterate butlast) (take-while identity) (map vec)))
 
 (defn infer-groups [items]
   (into items (comp
@@ -55,44 +71,52 @@
     ((fnil conj #{}) s k)))
 
 (defn tree-select
-  [& {:keys [model choices choice-renderer group-renderer group-label]
+  [& {:keys [model choice-renderer group-renderer]
       :or   {model           (r/atom nil)
              choice-renderer choice
              group-renderer  group}}]
-  (fn [& {:keys [choices group-label]}]
+  (fn [& {:keys [choices group-label-fn disabled? min-width max-width]
+          :or {group-label-fn {}}}]
     (let [choices (deref-or-value choices)
+          disabled? (deref-or-value disabled?)
           items   (->> choices
                        infer-groups
                        (sort-by (juxt (comp #(apply str %) :group)
                                       (complement group?))))
           item    (fn [{:keys [group id] :as m}]
-                    (if (group? m)
-                      [group-renderer
-                       (let [ancestors   (->> group ancestor-paths set)
-                             descendant? #(= group (vec (take (count group) (:group %))))
-                             descendants (->> choices
-                                              (filter descendant?)
-                                              (map :id))
-                             checked?    (cond
-                                           (every? (set (:choices @model)) descendants) :all
-                                           (some   (set (:choices @model)) descendants) :some)]
-                         (merge m {:label      (group-label (peek group))
-                                   :hide-show! #(swap! model update :groups toggle group)
-                                   :toggle!    #(swap! model update :choices (if (= :all checked?) clojure.set/difference (fnil into #{})) descendants)
-                                   :open?      (contains? (:groups @model) group)
-                                   :checked?   checked?
-                                   :model      model
-                                   :showing?   (every? (set (:groups @model)) (rest (ancestor-paths group)))
-                                   :level      (if (vector? group) (count group) 1)}))]
-                      [choice-renderer
-                       (merge m {:model    model
-                                 :showing? (every? (set (:groups @model)) (ancestor-paths group))
-                                 :toggle!  #(swap! model update :choices toggle id)
-                                 :checked? (some-> @model :choices (get id))
-                                 :level    (if (vector? group) (count group) 1)})]))]
+                    (let [group (as-v group)]
+                      (if (group? m)
+                        [group-renderer
+                         (let [descendant? #(= group (vec (take (count group) (as-v (:group %)))))
+                               descendants (->> choices
+                                                (filter descendant?)
+                                                (map :id))
+                               checked?    (cond
+                                             (every? (set (:choices @model)) descendants) :all
+                                             (some   (set (:choices @model)) descendants) :some)]
+                           (merge m {:label      (or (group-label-fn (peek group)) (peek group))
+                                     :hide-show! #(swap! model update :groups toggle group)
+                                     :toggle!    #(swap! model update :choices
+                                                         (if (= :all checked?) set/difference (fnil into #{}))
+                                                         descendants)
+                                     :open?      (contains? (:groups @model) group)
+                                     :checked?   checked?
+                                     :model      model
+                                     :disabled?  disabled?
+                                     :showing?   (every? (set (:groups @model)) (rest (ancestor-paths group)))
+                                     :level      (if (vector? group) (count group) 1)}))]
+                        [choice-renderer
+                         (merge m {:model    model
+                                   :showing? (if-not group
+                                               true
+                                               (every? (set (:groups @model)) (ancestor-paths group)))
+                                   :disabled? disabled?
+                                   :toggle!  #(swap! model update :choices toggle id)
+                                   :checked? (some-> @model :choices (get id))
+                                   :level    (count group)})])))]
       [:<>
-       (pr-str @model)
        [v-box
+        :min-width min-width
+        :max-width max-width
         :children
         (map item items)]])))
-
---
+++ src/re_demo/tree_select.cljs
@@ -0,0 +1,180 @@
+(ns re-demo.tree-select
+  (:require [cljs.pprint          :as pprint]
+            [clojure.string       :as string]
+            [reagent.core         :as reagent]
+            [re-com.core          :refer [at h-box box checkbox gap v-box tree-select tag-dropdown hyperlink-href p label line]]
+            [re-com.slider        :refer [slider]]
+            [re-com.tree-select   :refer [tree-select-parts-desc tree-select-args-desc]]
+            [re-com.tag-dropdown  :refer [tag-dropdown-parts-desc tag-dropdown-args-desc]]
+            [re-demo.utils        :refer [panel-title title2 title3 parts-table args-table github-hyperlink status-text new-in-version]]
+            [re-com.util          :refer [px]]))
+
+(def cities [{:id :sydney    :label "Sydney" :group [:oceania :australia :nsw]}
+             {:id :newcastle    :label "Newcastle" :group [:oceania :australia :nsw]}
+             {:id :melbourne :label "Melbourne" :group [:oceania :australia :victoria]}
+             {:id :christchurch :label "Christchurch" :group [:oceania :new-zealand :canterbury]}
+             {:id :auckland :label "Auckland" :group [:oceania :new-zealand]}
+             {:id :wellington :label "Wellington" :group [:oceania :new-zealand]}
+             {:id :atlantis :label "atlantis"}])
+
+(def choices [{:id :bug           :description "Something isn't working"                    :label "bug"           :background-color "#fc2a29" :group [:x :national :metro]}
+              {:id :documentation :description "Improvements or additions to documentation" :label "documentation" :background-color "#0052cc"}
+              {:id :duplicate     :description "This issue or pull request already exists"  :label "duplicate"     :background-color "#cccccc"}
+              {:id :enhancement   :description "New feature or request"                     :label "enhancement"   :background-color "#84b6eb"}
+              {:id :help          :description "Extra attention is needed"                  :label "help"          :background-color "#169819"}
+              {:id :invalid       :description "This doesn't seem right"                    :label "invalid"       :background-color "#e6e6e6"}
+              {:id :wontfix       :description "This will not be worked on"                 :label "wontfix"       :background-color "#eb6421"}])
+
+(defn demo
+  []
+  (let [model             (reagent/atom nil)
+        disabled?         (reagent/atom false)
+        placeholder?      (reagent/atom false)
+        abbrev-fn?        (reagent/atom false)
+        abbrev-threshold? (reagent/atom false)
+        abbrev-threshold  (reagent/atom 13)
+        min-width?        (reagent/atom true)
+        min-width         (reagent/atom 200)
+        max-width?        (reagent/atom true)
+        max-width         (reagent/atom 300)]
+    (fn []
+      [v-box :src (at)
+       :gap      "11px"
+       :width    "450px"
+       :align    :start
+       :children [[title2 "Demo"]
+                  [tree-select :src (at)
+                   :min-width         (when @min-width? (str @min-width "px"))
+                   :max-width         (when @max-width? (str @max-width "px"))
+                   :disabled?         disabled?
+                   :placeholder       (when @placeholder? "placeholder message")
+                   :choices           cities
+                   :model             model
+                   :abbrev-fn         (when @abbrev-fn? #(string/upper-case (first (:label %))))
+                   :abbrev-threshold  (when @abbrev-threshold? abbrev-threshold)
+                   :on-change         #(reset! model %)]
+                  [h-box :src (at)
+                   :height   "45px"
+                   :gap      "5px"
+                   :width    "100%"
+                   :children [[label :src (at) :label [:code ":model"]]
+                              [label :src (at) :label " is currently"]
+                              [:code
+                               {:class "display-flex"
+                                :style {:flex "1"}}
+                               (with-out-str (pprint/pprint @model))]]]
+                  [v-box :src (at)
+                   :gap "10px"
+                   :style {:min-width        "550px"
+                           :padding          "15px"
+                           :border-top       "1px solid #DDD"
+                           :background-color "#f7f7f7"}
+                   :children [[title3 "Interactive Parameters" {:margin-top "0"}]
+                              [v-box :src (at)
+                               :gap "20px"
+                               :children [[checkbox :src (at)
+                                           :label     [box :src (at)
+                                                       :align :start
+                                                       :child [:code ":disabled?"]]
+                                           :model     disabled?
+                                           :on-change #(reset! disabled? %)]
+                                          [checkbox :src (at)
+                                           :label     [box :src (at)
+                                                       :align :start
+                                                       :child [:span "Supply the string \"placeholder message\" for the " [:code ":placeholder"] " parameter"]]
+                                           :model     placeholder?
+                                           :on-change #(reset! placeholder? %)]
+                                          [v-box :src (at)
+                                           :gap      "11px"
+                                           :children [[checkbox :src (at)
+                                                       :label     [box :src (at)
+                                                                   :align :start
+                                                                   :child [:span "Supply an " [:code ":abbrev-fn"] " of " [:code "#(clojure.string/upper-case (first (:label %)))"]]]
+                                                       :model     abbrev-fn?
+                                                       :on-change #(reset! abbrev-fn? %)]
+                                                      (when @abbrev-fn?
+                                                        [h-box :src (at)
+                                                         :gap      "5px"
+                                                         :align    :center
+                                                         :children [[checkbox :src (at)
+                                                                     :label     [box :src (at)
+                                                                                 :align :start
+                                                                                 :child [:span " and also supply an " [:code ":abbrev-threshold"] " of "]]
+                                                                     :model     abbrev-threshold?
+                                                                     :on-change #(reset! abbrev-threshold? %)]
+                                                                    [slider
+                                                                     :model     abbrev-threshold
+                                                                     :on-change #(reset! abbrev-threshold %)
+                                                                     :min       10
+                                                                     :max       50
+                                                                     :step      1
+                                                                     :width     "160px"]
+                                                                    [label :src (at) :label @abbrev-threshold]]])]]
+                                          [h-box :src (at)
+                                           :align    :center
+                                           :children [[checkbox :src (at)
+                                                       :label     [box :src (at)
+                                                                   :align :start
+                                                                   :child [:code ":min-width"]]
+                                                       :model     min-width?
+                                                       :on-change #(reset! min-width? %)]
+                                                      [gap :src (at) :size "5px"]
+                                                      (when @min-width?
+                                                        [:<>
+                                                         [slider
+                                                          :model     min-width
+                                                          :on-change #(reset! min-width %)
+                                                          :min       50
+                                                          :max       400
+                                                          :step      1
+                                                          :width     "300px"]
+                                                         [gap :src (at) :size "5px"]
+                                                         [label :src (at) :label (str @min-width "px")]])]]
+                                          [h-box :src (at)
+                                           :align    :center
+                                           :children [[checkbox :src (at)
+                                                       :label     [box :src (at)
+                                                                   :align :start
+                                                                   :child [:code ":max-width"]]
+                                                       :model     max-width?
+                                                       :on-change #(reset! max-width? %)]
+                                                      [gap :src (at) :size "5px"]
+                                                      (when @max-width?
+                                                        [:<>
+                                                         [slider
+                                                          :model     max-width
+                                                          :on-change #(reset! max-width %)
+                                                          :min       50
+                                                          :max       400
+                                                          :step      1
+                                                          :width     "300px"]
+                                                         [gap :src (at) :size "5px"]
+                                                         [label :src (at) :label (str @max-width "px")]])]]]]]]
+                  #_[gap :src (at) :size "5px"]
+
+                  [gap :src (at) :size "10px"]]])))
+
+(defn panel []
+  [v-box :src (at)
+   :size     "auto"
+   :gap      "10px"
+   :children [[panel-title "[tree-select ... ]"
+               "src/re_com/tree-select.cljs"
+               "src/re_demo/tree-select.cljs"]
+
+              [h-box :src (at)
+               :gap      "100px"
+               :children [[v-box :src (at)
+                           :gap      "10px"
+                           :width    "450px"
+                           :children [[title2 "Notes"]
+                                      [status-text "Alpha" {:color "red" :font-weight "bold"}]
+                                      [new-in-version "v2.13.0"]
+                                      [p "A multi-select component. Useful when the list of choices is small and (optionally) colour coded, and where those selected need to all be visible to the user."]
+                                      [p "If the user selects many of the choices, then displaying them horizontally can take more than " [:code ":width"] ". In this case, the programmer has two strategies:"]
+                                      [:ol
+                                       [:li  "allow the Component to grow horizontally to some limit by providing " [:code ":max-width"]]
+                                       [:li  "allow the Component to switch from using \"name\" to using \"abrreviations\", see " [:code ":abbrev-fn"] " and  " [:code ":abbrev-threshold"]]]
+                                      [args-table tree-select-args-desc]]]
+                          [demo]]]
+              [parts-table "tag-dropdown" tree-select-parts-desc]]])
