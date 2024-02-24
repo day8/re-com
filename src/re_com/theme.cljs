0f73969e4c72ab5037bc90605854bef1732cc018
Kimo Knowles <kimo.knowles@day8.com.au>
2024-02-24 21:27:16 +0100

    Use themes more simply, for variables & parts

--- src/re_com/theme.cljs
+++ src/re_com/theme.cljs
@@ -5,33 +5,30 @@
 
 (def global (r/atom []))
 
-(defn merge-attr [& ms]
+(defn merge-props [& ms]
   (let [class-vec #(if (vector? %) % [%])
         ms (remove nil? ms)
         ms (map #(cond-> % (and (map? %)
                                 (:class %))
                          (update :class class-vec)) ms)]
     (cond
-      (every? map? ms) (clojure.core/apply merge-with merge-attr ms)
+      (every? map? ms) (clojure.core/apply merge-with merge-props ms)
       (every? vector? ms) (reduce into ms)
       :else (last ms))))
 
-(defn parts [parts attr _state part]
-  (let [current-part (get parts part)]
-    (cond-> attr current-part (merge-attr current-part))))
+(defn parts [part->props]
+  (fn [props {:keys [part]}]
+    (merge-props props (get part->props part))))
 
-(defn apply-parts [attr parts-spec part]
-  (parts parts-spec attr nil part))
-
-(defn rf [[attr ctx] theme]
-  (let [result (theme attr ctx)]
+(defn rf [[props ctx] theme]
+  (let [result (theme props ctx)]
     (if (vector? result) result [result ctx])))
 
 (defn apply
-  ([attr ctx & themes]
+  ([props ctx & themes]
    (->> themes
         (into @global)
         flatten
         (remove nil?)
-        (reduce rf [attr ctx])
+        (reduce rf [props ctx])
         first)))
