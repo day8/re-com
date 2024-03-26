e1ba518d8c9e1be435dc590fc8819f2a652e70cc
Kimo Knowles <kimo.knowles@day8.com.au>
2024-03-26 16:02:00 +0100

    Upgrade shadow-cljs to 2.28.2

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,6 +1,6 @@
 {:npm-dev-deps {"react"                 "17.0.2"
                 "react-dom"             "17.0.2"
-                "shadow-cljs"           "2.15.2"
+                "shadow-cljs"           "2.28.2"
                 "karma"                 "6.3.4"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
