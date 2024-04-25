cb0cc15b08d8965d4fdcd502768dfc5489e8213d
Kimo Knowles <kimo.knowles@day8.com.au>
2024-04-25 09:17:32 +0200

    [wip] test CI

--- karma.conf.js
+++ karma.conf.js
@@ -1,32 +1,37 @@
 module.exports = function (config) {
-  var root = './target/karma' // same as :output-dir
-  var junitOutputDir = "target/karma/junit"
-  var browsers = ['ChromeHeadless']
+    var root = './target/karma' // same as :output-dir
+    var junitOutputDir = "target/karma/junit"
 
-  config.set({
-    frameworks: ['cljs-test'],
-    browsers: browsers,
-    basePath: root,
-    files: [
-      'test.js'
-    ],
-    plugins: [
-        'karma-cljs-test',
-        'karma-chrome-launcher',
-        'karma-junit-reporter'
-    ],
-    colors: true,
-    logLevel: config.LOG_INFO,
-    client: {
-      args: ['shadow.test.karma.init'],
-      singleRun: true
-    },
+    config.set({
+        frameworks: ['cljs-test'],
+        basePath: root,
+        files: [
+            'test.js'
+        ],
+        plugins: [
+            'karma-cljs-test',
+            'karma-chrome-launcher',
+            'karma-junit-reporter'
+        ],
+        colors: true,
+        logLevel: config.LOG_INFO,
+        client: {
+            args: ['shadow.test.karma.init'],
+            singleRun: true
+        },
+        customLaunchers: {
+            ChromeHeadlessNoSandbox: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox', '--disable-setuid-sandbox']
+            }
+        },
+        browsers: ['ChromeHeadlessNoSandbox'],
 
-    // the default configuration
-    junitReporter: {
-      outputDir: junitOutputDir + '/karma', // results will be saved as outputDir/browserName.xml
-      outputFile: undefined, // if included, results will be saved as outputDir/browserName/outputFile
-      suite: '' // suite will become the package name attribute in xml testsuite element
-    }
-  })
+        // the default configuration
+        junitReporter: {
+            outputDir: junitOutputDir + '/karma', // results will be saved as outputDir/browserName.xml
+            outputFile: undefined, // if included, results will be saved as outputDir/browserName/outputFile
+            suite: '' // suite will become the package name attribute in xml testsuite element
+        }
+    })
 }
