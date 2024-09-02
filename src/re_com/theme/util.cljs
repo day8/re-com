30430b76eb4c69b4c692ef1c7a366aadd7b7d486
Kimo Knowles <kimo.knowles@day8.com.au>
2024-09-02 23:01:44 +0200

    Optimize merge-props

--- src/re_com/theme/util.cljs
+++ src/re_com/theme/util.cljs
@@ -1,12 +1,23 @@
 (ns re-com.theme.util
   (:require [re-com.util :as u]))
 
-(defn merge-props [& ms]
-  (let [ms (remove nil? ms)
-        ms (map #(cond-> % (and (map? %)
-                                (:class %))
-                         (update :class u/->v)) ms)]
-    (cond
-      (every? map? ms) (clojure.core/apply merge-with merge-props ms)
-      (every? vector? ms) (reduce into ms)
-      :else (last ms))))
+#_(defn merge-props [& ms]
+    (let [ms (remove nil? ms)
+          ms (map #(cond-> % (and (map? %)
+                                  (:class %))
+                           (update :class u/->v)) ms)]
+      (cond
+        (every? map? ms) (clojure.core/apply merge-with merge-props ms)
+        (every? vector? ms) (reduce into ms)
+        :else (last ms))))
+
+(defn rf [acc {:keys [class attr style] :as m}]
+  (merge acc (cond-> (if-not (string? m) m {:style [m]})
+               class
+               (assoc :class (into (u/->v (:class acc)) (u/->v class)))
+               attr
+               (assoc :attr (merge (:attr acc) attr))
+               style
+               (assoc :style (merge (:style acc) style)))))
+
+(defn merge-props [& ms] (reduce rf {} ms))
