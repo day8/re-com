b4dac4c37be199540989574592ae5b07ab90ebc3
Kimo Knowles <kimo.knowles@day8.com.au>
2025-03-14 22:00:30 +0100

    Update blue-moderne theme: de-emphasize dropdown indicators

--- src/re_com/theme/blue_modern.cljs
+++ src/re_com/theme/blue_modern.cljs
@@ -6,9 +6,13 @@
    [re-com.tree-select :as tree-select]
    [re-com.error-modal :as-alias error-modal]))
 
-(defn theme [{:as props :keys [part]}]
+(defn theme [{:as            props
+              :keys          [part]
+              {$ :variables} :re-com}]
   (->> (case part
          ::dropdown/anchor-wrapper
-         {:style {:height      "25px"}}
+         {:style {:height "25px"}}
+         ::dropdown/indicator
+         {:style {:color (:light-foreground $)}}
          {})
        (theme/merge-props props)))
