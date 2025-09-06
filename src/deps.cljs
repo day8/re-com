559c62110009b662402ed284628a8009d0464586
Kimo Knowles <kimo.knowles@day8.com.au>
2025-09-06 22:55:04 +0200

    Update test runner

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,7 +1,3 @@
-{:npm-dev-deps {"react"                 "17.0.2"
-                "react-dom"             "17.0.2"
-                "shadow-cljs"           "2.28.2"
-                "karma"                 "6.3.4"
-                "karma-chrome-launcher" "3.1.0"
-                "karma-cljs-test"       "0.1.0"
-                "karma-junit-reporter"  "2.0.1"}}
+{:npm-dev-deps {"react"       "17.0.2"
+                "react-dom"   "17.0.2"
+                "shadow-cljs" "2.28.2"}}
