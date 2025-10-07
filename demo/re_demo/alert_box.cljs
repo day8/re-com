8e52e4db9b104120f12ccfff7b97e9771822010f
kk <kk@charcoal>
2025-10-07 23:30:18 +0200

    [format]

--- demo/re_demo/alert_box.cljs
+++ demo/re_demo/alert_box.cljs
@@ -22,158 +22,165 @@
        :src      (at)
        :size     "auto"
        :gap      "10px"
-       :children [[panel-title "[alert-box ... ]"
-                   "src/re_com/alert.cljs"
-                   "demo/re_demo/alert_box.cljs"]
+       :children
+       [[panel-title "[alert-box ... ]"
+         "src/re_com/alert.cljs"
+         "demo/re_demo/alert_box.cljs"]
 
-                  [h-box
-                   :src      (at)
-                   :gap      "100px"
-                   :children [[v-box
-                               :src      (at)
-                               :gap      "10px"
-                               :width    "450px"
-                               :children [[title2 "Notes"]
-                                          [status-text "Stable"]
-                                          [p "A component which renders a single bootstrap styled alert-box."]
-                                          [args-table alert-box-args-desc]]]
-                              [v-box
-                               :src      (at)
-                               :width    "600px"
-                               :gap      "10px"
-                               :children [[title2 "Demo"]
-                                          (if @show-alert1
-                                            [alert-box      ;(alert-box-meta alert-box)
-                                             :src        (at)
-                                             :id         1
-                                             :alert-type :info
-                                             :heading    "This Is An Alert Heading"
-                                             :body       [:p "This is an alert body. This alert has an :alert-type of :info which makes it green, and it includes a :heading, a :body and a close button. Click the x to close it."]
-                                             :closeable? true
-                                             :on-close   #(reset! show-alert1 false)]
-                                            [:p {:style {:text-align "center" :margin "30px"}} "[You closed me]"])
+        [h-box
+         :src      (at)
+         :gap      "100px"
+         :children
+         [[v-box
+           :src      (at)
+           :gap      "10px"
+           :width    "450px"
+           :children
+           [[title2 "Notes"]
+            [status-text "Stable"]
+            [p "A component which renders a single bootstrap styled alert-box."]
+            [args-table alert-box-args-desc]]]
+          [v-box
+           :src      (at)
+           :width    "600px"
+           :gap      "10px"
+           :children
+           [[title2 "Demo"]
+            (if @show-alert1
+              [alert-box
+               {:src        (at)
+                :id         1
+                :alert-type :info
+                :heading    "This Is An Alert Heading"
+                :body       [:p "This is an alert body. This alert has an :alert-type "
+                             "of :info which makes it green, and it includes a :heading, "
+                             "a :body and a close button. Click the x to close it."]
+                :closeable? true
+                :on-close   #(reset! show-alert1 false)}]
+              [:p {:style {:text-align "center" :margin "30px"}} "[You closed me]"])
 
-                                          [gap
-                                           :src  (at)
-                                           :size "30px"]
-                                          [title
-                                           :src   (at)
-                                           :level :level3
-                                           :label "Further Variations"]
-                                          (when @show-alert2
-                                            [:div
-                                             [alert-box
-                                              :src        (at)
-                                              :alert-type :info
-                                              :heading    "Alert with :heading but no :body"
-                                              :closeable? true
-                                              :on-close   #(reset! show-alert2 false)]])
-                                          (when @show-alert3
-                                            [:div
-                                             [alert-box
-                                              :src        (at)
-                                              :alert-type :warning
-                                              :body       "Alert with :body but no :heading (:padding set to 6px)."
-                                              :padding    "6px"
-                                              :closeable? true
-                                              :on-close   #(reset! show-alert3 false)]])
-                                          [alert-box
-                                           :src        (at)
-                                           :alert-type :danger
-                                           :heading    ":alert-type is :danger"
-                                           :body       [:span "This is the :body of an danger-styled alert with :closeable? omitted (defaults to false). "
-                                                        [:a {:href "http://google.com" :target "_blank"} "Link to Google"] "."]]
-                                          [gap
-                                           :src  (at)
-                                           :size "30px"]
-                                          [title
-                                           :src   (at)
-                                           :level :level3
-                                           :label [:span [:code ":alert-type"] " set to " [:code ":none"]]]
-                                          (when @show-alert4
-                                            [alert-box
-                                             :src        (at)
-                                             :id         1
-                                             :alert-type :none
-                                             :heading    "This Is An Unstyled Alert"
-                                             :body       [:p "This is an alert body. This alert has an :alert-type of :none, and it includes a :heading, a :body and a close button. Click the x to close it."]
-                                             :closeable? true
-                                             :on-close   #(reset! show-alert4 false)])
+            [gap
+             :src  (at)
+             :size "30px"]
+            [title
+             :src   (at)
+             :level :level3
+             :label "Further Variations"]
+            (when @show-alert2
+              [:div
+               [alert-box
+                :src        (at)
+                :alert-type :info
+                :heading    "Alert with :heading but no :body"
+                :closeable? true
+                :on-close   #(reset! show-alert2 false)]])
+            (when @show-alert3
+              [:div
+               [alert-box
+                :src        (at)
+                :alert-type :warning
+                :body       "Alert with :body but no :heading (:padding set to 6px)."
+                :padding    "6px"
+                :closeable? true
+                :on-close   #(reset! show-alert3 false)]])
+            [alert-box
+             :src        (at)
+             :alert-type :danger
+             :heading    ":alert-type is :danger"
+             :body       [:span "This is the :body of an danger-styled alert with :closeable? omitted (defaults to false). "
+                          [:a {:href "http://google.com" :target "_blank"} "Link to Google"] "."]]
+            [gap
+             :src  (at)
+             :size "30px"]
+            [title
+             :src   (at)
+             :level :level3
+             :label [:span [:code ":alert-type"] " set to " [:code ":none"]]]
+            (when @show-alert4
+              [alert-box
+               :src        (at)
+               :id         1
+               :alert-type :none
+               :heading    "This Is An Unstyled Alert"
+               :body       [:p "This is an alert body. This alert has an :alert-type of :none, and it includes a :heading, a :body and a close button. Click the x to close it."]
+               :closeable? true
+               :on-close   #(reset! show-alert4 false)])
 
-                                          [title
-                                           :src   (at)
-                                           :level :level3
-                                           :label [:span [:code ":alert-type"] " set to " [:code ":none"] " with custom " [:code ":style"] " and " [:code ":body"]]]
+            [title
+             :src   (at)
+             :level :level3
+             :label [:span [:code ":alert-type"] " set to " [:code ":none"] " with custom " [:code ":style"] " and " [:code ":body"]]]
 
-                                          (when @show-alert5
-                                            [:div
-                                             [alert-box
-                                              :src        (at)
-                                              :alert-type :none
-                                              :style      {:color             "#222"
-                                                           :background-color  "#eff9e3"
-                                                           :border-top        "none"
-                                                           :border-right      "none"
-                                                           :border-bottom     "none"
-                                                           :border-left       "4px solid green"
-                                                           :border-radius     "0px"}
-                                              :heading    "Alert with :heading but no :body"
-                                              :closeable? true
-                                              :on-close   #(reset! show-alert5 false)]])
-                                          (when @show-alert6
-                                            [:div
-                                             [alert-box
-                                              :src        (at)
-                                              :alert-type :none
-                                              :style      {:color             "#222"
-                                                           :background-color  "rgba(255, 165, 0, 0.1)"
-                                                           :border-top        "none"
-                                                           :border-right      "none"
-                                                           :border-bottom     "none"
-                                                           :border-left       "4px solid rgba(255, 165, 0, 0.8)"
-                                                           :border-radius     "0px"}
-                                              :body       "Alert with :body but no :heading (:padding set to 6px)."
-                                              :padding    "6px"
-                                              :closeable? true
-                                              :on-close   #(reset! show-alert6 false)]])
-                                          [alert-box
-                                           :src        (at)
-                                           :alert-type :none
-                                           :style      {:color             "#333"
-                                                        :background-color  "rgba(255, 0, 0, 0.1)"
-                                                        :border-top        "none"
-                                                        :border-right      "none"
-                                                        :border-bottom     "none"
-                                                        :border-left       "4px solid rgba(255, 0, 0, 0.8)"
-                                                        :border-radius     "0px"}
-                                           :heading    ":alert-type is :danger"
-                                           :body       [:span "This is the :body of an danger-styled alert with :closeable? omitted (defaults to false). "
-                                                        [:a {:href "http://google.com" :target "_blank"} "Link to Google"] "."]]
+            (when @show-alert5
+              [:div
+               [alert-box
+                :src        (at)
+                :alert-type :none
+                :style      {:color            "#222"
+                             :background-color "#eff9e3"
+                             :border-top       "none"
+                             :border-right     "none"
+                             :border-bottom    "none"
+                             :border-left      "4px solid green"
+                             :border-radius    "0px"}
+                :heading    "Alert with :heading but no :body"
+                :closeable? true
+                :on-close   #(reset! show-alert5 false)]])
+            (when @show-alert6
+              [:div
+               [alert-box
+                :src        (at)
+                :alert-type :none
+                :style      {:color            "#222"
+                             :background-color "rgba(255, 165, 0, 0.1)"
+                             :border-top       "none"
+                             :border-right     "none"
+                             :border-bottom    "none"
+                             :border-left      "4px solid rgba(255, 165, 0, 0.8)"
+                             :border-radius    "0px"}
+                :body       "Alert with :body but no :heading (:padding set to 6px)."
+                :padding    "6px"
+                :closeable? true
+                :on-close   #(reset! show-alert6 false)]])
+            [alert-box
+             :src        (at)
+             :alert-type :none
+             :style      {:color            "#333"
+                          :background-color "rgba(255, 0, 0, 0.1)"
+                          :border-top       "none"
+                          :border-right     "none"
+                          :border-bottom    "none"
+                          :border-left      "4px solid rgba(255, 0, 0, 0.8)"
+                          :border-radius    "0px"}
+             :heading    ":alert-type is :danger"
+             :body       [:span "This is the :body of an danger-styled alert with :closeable? omitted (defaults to false). "
+                          [:a {:href "http://google.com" :target "_blank"} "Link to Google"] "."]]
 
-                                          [alert-box
-                                           :src        (at)
-                                           :id         1
-                                           :alert-type :none
-                                           :body       [h-box
-                                                        :src      (at)
-                                                        :gap      "10px"
-                                                        :children [[box
-                                                                    :src   (at)
-                                                                    :child [:span "Last scan: 6/8/2015, 1:46:10 PM" [:br] "Scanned in 5.21s"]]
-                                                                   [line
-                                                                    :src  (at)
-                                                                    :size "2px" :color "green"]
-                                                                   [box
-                                                                    :src   (at)
-                                                                    :child [:span "Vendor:" [:br] "Model:"]]]]
-                                           :style      {:background-color "rgba(223, 240, 200, 0.4)"
-                                                        :border           "2px solid green"
-                                                        :border-radius    "0px"
-                                                        :box-shadow       "2px 2px 6px #ccc"}]
-                                          [gap
-                                           :src  (at)
-                                           :size "60px"]]]]]
-                  [parts-table "alert-box" alert-box-parts-desc]]])))
+            [alert-box
+             :src        (at)
+             :id         1
+             :alert-type :none
+             :body       [h-box
+                          :src      (at)
+                          :gap      "10px"
+                          :children
+                          [[box
+                            :src   (at)
+                            :child [:span "Last scan: 6/8/2015, 1:46:10 PM" [:br] "Scanned in 5.21s"]]
+                           [line
+                            :src  (at)
+                            :size "2px" :color "green"]
+                           [box
+                            :src   (at)
+                            :child [:span "Vendor:" [:br] "Model:"]]]]
+             :style      {:background-color "rgba(223, 240, 200, 0.4)"
+                          :border           "2px solid green"
+                          :border-radius    "0px"
+                          :box-shadow       "2px 2px 6px #ccc"}]
+            [gap
+             :src  (at)
+             :size "60px"]]]]]
+        [parts-table "alert-box" alert-box-parts-desc]]])))
 
 ;; core holds onto references, so need one level of indirection to get figwheel updates
 (defn panel
